36cbcc9ed9cc2f41354a486012d7863e8081447a
averbraeck <averbraeck@6bd9694c-b1be-467f-8798-70e1d16d5ec4>
2020-02-10 14:43:56 +0000

    In order to prevent name clashes for the EventType, the full name of the class from which the EventType was defined (usually in the clinit method) is added to the equals() and hashCode() methods of the EventType. In that way, EventTypes that are the same will be unique, but EventTypes with just the same name but defined in different classes will be different. The reason why this is important is because remote events that use EventTypes can have multiple versions of the same public static final EventType: one the is defined in the client, and one that is defined via the network. These will have different addresses in memory but they share the same class and name info, so equals() will yield true.

--- trunk/djutils/src/main/java/org/djutils/event/EventType.java
+++ trunk/djutils/src/main/java/org/djutils/event/EventType.java
@@ -4,9 +4,14 @@ import java.io.Serializable;
 
 /**
  * The EventType is a masker used for the subscription to asynchronous events. Eventtypes are used by EventProducers to show
- * which events they potentially fire. EventTypes should be defined as static final fields. The EventType does not have a
- * hashCode based on its name anymore, because in that case an expensive check should be done to search for name clashes in the
- * class hierarchy. By using the 'address' of the object, different EventTypes have by definition a unique hashCode. 
+ * which events they potentially fire. EventTypes should be defined as static final fields. In order to prevent name clashes for
+ * the EventType, the full name of the class from which the EventType was defined (usually in the &lt;clinit&gt;) is added to
+ * the equals() and hashCode() methods of the EventType. In that way, EventTypes that are the same will be unique, but
+ * EventTypes with just the same name but defined in different classes will be different. <br>
+ * <br>
+ * Note: the reason why this is important is because <b>remote events</b> that use EventTypes can have <i>multiple versions</i>
+ * of the same public static final EventType: one the is defined in the client, and one that is defined via the network. These
+ * will have <i>different addresses in memory</i> but they share the same class and name info, so equals() will yield true.
  * <p>
  * Copyright (c) 2002-2020 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
@@ -25,11 +30,16 @@ public final class EventType implements Serializable
 
     /** The name of the eventType. */
     private final String name;
-    
+
+    /**
+     * The class name from which the event type construction was called; together with the event name this should lead to a
+     * unique hash, even when the same name is used in different classes.
+     */
+    private final String definingClassName;
+
     /**
      * Construct a new EventType.
-     * @param name String; the name of this eventType. Two values are not appreciated : &lt;code&gt;null&lt;/code&gt; and
-     *            &lt;code&gt;""&lt;/code&gt;.
+     * @param name String; the name of this eventType. Two values are not appreciated: null and the empty string.
      */
     public EventType(final String name)
     {
@@ -38,6 +48,8 @@ public final class EventType implements Serializable
             throw new IllegalArgumentException("EventType name == null || EventType name == \"\"");
         }
         this.name = name;
+        StackTraceElement[] steArray = new Throwable().getStackTrace();
+        this.definingClassName = steArray[1].getClassName();
     }
 
     /**
@@ -55,4 +67,35 @@ public final class EventType implements Serializable
     {
         return this.name;
     }
+
+    /** {@inheritDoc} */
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + this.definingClassName.hashCode();
+        result = prime * result + this.name.hashCode();
+        return result;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("checkstyle:needbraces")
+    public boolean equals(final Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        EventType other = (EventType) obj;
+        if (!this.name.equals(other.name))
+            return false;
+        if (!this.definingClassName.equals(other.definingClassName))
+            return false;
+        return true;
+    }
+
 }
