0d9af3d0df929f360e7bb7c0c95d7ea48d009738
pknoppers <pknoppers@6bd9694c-b1be-467f-8798-70e1d16d5ec4>
2020-03-18 10:06:58 +0000

    Minor javadoc edits.

--- trunk/djutils-data/src/main/java/org/djutils/data/csv/TSVData.java
+++ trunk/djutils-data/src/main/java/org/djutils/data/csv/TSVData.java
@@ -73,7 +73,7 @@ public final class TSVData
     }
 
     /**
-     * Read the data from the tsv-file into the data table. Use the metadata to reconstruct the data table.
+     * Read the data from the TSV-file into the data table. Use the metadata to reconstruct the data table.
      * @param reader Reader; the reader that can read the data, e.g. from a file
      * @param metaReader Reader; the writer for the metadata
      * @return dataTable the data table reconstructed from the meta data and filled with the data
@@ -88,7 +88,7 @@ public final class TSVData
     }
 
     /**
-     * Read the data from the tsv-file into the data table. Use the metadata to reconstruct the data table.
+     * Read the data from the TSV-file into the data table. Use the metadata to reconstruct the data table.
      * @param filename String; the file name to read the data from
      * @param metaFilename String; the file name to read the metadata from
      * @return dataTable the data table reconstructed from the meta data and filled with the data
