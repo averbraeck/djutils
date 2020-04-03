0534cddc389e3280065e95776d27ea4eb985e938
averbraeck <averbraeck@6bd9694c-b1be-467f-8798-70e1d16d5ec4>
2020-04-03 14:21:39 +0000

    deployed version 1.04.15

---
+++ release/1.04.15/djutils/src/main/java/org/djutils/metadata/MetaData.java
@@ -0,0 +1,174 @@
+package org.djutils.metadata;
+
+import java.util.Arrays;
+
+import org.djutils.exceptions.Throw;
+
+/**
+ * MetaDataInterface; documenting Object arrays. <br>
+ * Copyright (c) 2020-2020 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>. <br>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="https://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class MetaData
+{
+    /** Name of this MetaData object. */
+    private final String name;
+
+    /** Description of this MetaData object. */
+    private final String description;
+
+    /** The field descriptors. */
+    private final ObjectDescriptor[] objectDescriptors;
+
+    /** The single field descriptor. */
+    private final ObjectDescriptor objectDescriptor;
+
+    /**
+     * Construct a new MetaData object that can check an array of Object.
+     * @param name String; name of the new MetaData object
+     * @param description String; description of the new MetaData object
+     * @param objectDescriptors ObjectDescriptor[]; array of ObjectDescriptor. This constructor does <b>not</b> make a deep copy
+     *            of this array; subsequent modification of the contents of the provided <code>objectDescriptors</code> array
+     *            will affect the behavior of the MetaData object.
+     */
+    public MetaData(final String name, final String description, final ObjectDescriptor[] objectDescriptors)
+    {
+        Throw.whenNull(name, "name may not be null");
+        Throw.whenNull(description, "description may not be null");
+        Throw.whenNull(objectDescriptors, "objectDescriptors may not be null");
+        this.name = name;
+        this.description = description;
+        this.objectDescriptors = objectDescriptors;
+        this.objectDescriptor = null;
+    }
+
+    /**
+     * Construct a new MetaData object that can check a single Object..
+     * @param name String; name of the new MetaData object
+     * @param description String; description of the new MetaData object
+     * @param objectDescriptor ObjectDescriptor; the descriptor for the object that the new MetaData object will accept as valid
+     */
+    public MetaData(final String name, final String description, final ObjectDescriptor objectDescriptor)
+    {
+        Throw.whenNull(name, "name may not be null");
+        Throw.whenNull(description, "description may not be null");
+        Throw.whenNull(objectDescriptor, "objectDescriptor may not be null");
+        this.name = name;
+        this.description = description;
+        this.objectDescriptors = null;
+        this.objectDescriptor = objectDescriptor;
+    }
+
+    /**
+     * Retrieve the name of this MetaData object.
+     * @return String; the name of this MetaData object
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    /**
+     * Retrieve the description of this MetaData object.
+     * @return String; the description of this MetaData object
+     */
+    public String getDescription()
+    {
+        return this.description;
+    }
+
+    /**
+     * Retrieve the length of described Object array.
+     * @return int; the length of the described Object array; returns 0 if this MetaDataObject is not set up to validate an
+     *         array of Object.
+     */
+    public int size()
+    {
+        return null == this.objectDescriptors ? 0 : this.objectDescriptors.length;
+    }
+
+    /**
+     * Retrieve the name of one element in the Object array.
+     * @param index int; index of the element in the Object array (must be 0 if this MetaData object is not set up to validate
+     *            an array of Object)
+     * @return String; name of the argument
+     */
+    public String getFieldName(final int index)
+    {
+        return getObjectDescriptor(index).getName();
+    }
+
+    /**
+     * Retrieve the description of one element in the Object array.
+     * @param index int; index of the element in the Object array (must be 0 if this MetaData object is not set up to validate
+     *            an array of Object)
+     * @return String; description of the argument
+     */
+    public String getFieldDescription(final int index)
+    {
+        return getObjectDescriptor(index).getDescription();
+    }
+
+    /**
+     * Retrieve the java class of one element in the Object array.
+     * @param index int; index of the element in the Object array (must be 0 if this MetaData object is not set up to validate
+     *            an array of Object)
+     * @return Class&lt;?&gt;; java class of the element
+     */
+    public Class<?> getFieldClass(final int index)
+    {
+        return getObjectDescriptor(index).getObjectClass();
+    }
+
+    /**
+     * Select one of the ObjectDescriptors.
+     * @param index int; index of the ObjectDescriptor (must be 0 in case this MetaData object is not set up to validate an
+     *            array of Object)
+     * @return ObjectDescriptor; the selected ObjectDescriptor
+     */
+    private ObjectDescriptor getObjectDescriptor(final int index)
+    {
+        if (null == this.objectDescriptors)
+        {
+            Throw.when(index != 0, IndexOutOfBoundsException.class, "Index must be 0");
+            return this.objectDescriptor;
+        }
+        return this.objectDescriptors[index];
+    }
+
+    /**
+     * Verify that an Object array has the prescribed composition.
+     * @param objectArray Object[]; the Object array to verify. If the array is supposed to have 0 length, a null pointer is
+     *            deemed OK.
+     */
+    public final void verifyComposition(final Object[] objectArray)
+    {
+        if (size() == 0 && objectArray == null)
+        {
+            return;
+        }
+        Throw.whenNull(objectArray, "objectArray may not be null");
+        Throw.when(objectArray.length != size(), IndexOutOfBoundsException.class, "objectArray has wrong length");
+        for (int index = 0; index < objectArray.length; index++)
+        {
+            if (!(getFieldClass(index).isAssignableFrom(objectArray[index].getClass())))
+            {
+                throw new ClassCastException(String.format("objectArray[%d] (%s) cannot be used for %s", index,
+                        objectArray[index], getFieldClass(index).getName()));
+            }
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "MetaData [name=" + this.name + ", description=" + this.description + ", objectDescriptors="
+                + Arrays.toString(this.objectDescriptors) + "]";
+    }
+
+}
---
+++ release/1.04.15/djutils/src/test/java/org/djutils/metadata/MetaDataTest.java
@@ -0,0 +1,299 @@
+package org.djutils.metadata;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * MetaDataTest.java. <br>
+ * <br>
+ * Copyright (c) 2020-2020 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>. <br>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="https://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class MetaDataTest
+{
+
+    /**
+     * Test the ObjectDescriptor class.
+     */
+    @Test
+    public void testObjectDescriptor()
+    {
+        ObjectDescriptor objectDescriptor = new ObjectDescriptor("name", "description", Integer.class);
+        assertEquals("name", "name", objectDescriptor.getName());
+        assertEquals("description", "description", objectDescriptor.getDescription());
+        assertEquals("class", Integer.class, objectDescriptor.getObjectClass());
+        try
+        {
+            new ObjectDescriptor(null, "description", Integer.class);
+            fail("null name should have thrown a NullPointerException");
+        }
+        catch (NullPointerException npe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            new ObjectDescriptor("name", null, Integer.class);
+            fail("null description should have thrown a NullPointerException");
+        }
+        catch (NullPointerException npe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            new ObjectDescriptor("name", "description", null);
+            fail("null class should have thrown a NullPointerException");
+        }
+        catch (NullPointerException npe)
+        {
+            // Ignore expected exception
+        }
+
+        assertTrue("toString returns something descriptive", objectDescriptor.toString().startsWith("ObjectDescriptor"));
+    }
+
+    /**
+     * Test the MetaData class.
+     */
+    @Test
+    public void testMetaData()
+    {
+        // Construct a MetaData object that specifies a String and a Double.
+        MetaData metaData = new MetaData("meta data name", "meta data description",
+                new ObjectDescriptor[] { new ObjectDescriptor("string", "the string", String.class),
+                        new ObjectDescriptor("length", "the length", Double.class) });
+        assertEquals("name", "meta data name", metaData.getName());
+        assertEquals("description", "meta data description", metaData.getDescription());
+        assertEquals("size", 2, metaData.size());
+        assertEquals("name of element 0", "string", metaData.getFieldName(0));
+        assertEquals("name of element 1", "length", metaData.getFieldName(1));
+        assertEquals("description of element 0", "the string", metaData.getFieldDescription(0));
+        assertEquals("description of element 1", "the length", metaData.getFieldDescription(1));
+        assertEquals("class of element 0", String.class, metaData.getFieldClass(0));
+        assertEquals("class of element 1", Double.class, metaData.getFieldClass(1));
+        assertTrue("toString returns something descriptive", metaData.toString().startsWith("MetaData"));
+        metaData.verifyComposition(new Object[] { "TestString", 123.456 });
+        try
+        {
+            metaData.verifyComposition(new Object[] { "TestString" }); // too short
+            fail("Too short array should have thrown an IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException ioobe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            metaData.verifyComposition(new Object[] { "TestString", 123.456, "too many" }); // too long
+            fail("Too long array should have thrown an IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException ioobe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            metaData.verifyComposition(new Object[] { 234.678, 123.456 }); // element 0 not a String
+            fail("Wrong class should have thrown an ClassCastException");
+        }
+        catch (ClassCastException cce)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            metaData.verifyComposition(new Object[] { "TestString", "wrong class" }); // element 1 not a Double
+            fail("Wrong class should have thrown an IndexOutOfBoundsException");
+        }
+        catch (ClassCastException cce)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            metaData.getFieldName(-1);
+            fail("Bad index should have thrown an IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException ioobe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            metaData.getFieldName(2);
+            fail("Bad index should have thrown an IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException ioobe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            metaData.getFieldDescription(-1);
+            fail("Bad index should have thrown an IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException ioobe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            metaData.getFieldDescription(2);
+            fail("Bad index should have thrown an IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException ioobe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            metaData.getFieldClass(-1);
+            fail("Bad index should have thrown an IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException ioobe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            metaData.getFieldClass(2);
+            fail("Bad index should have thrown an IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException ioobe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            new MetaData(null, "description", new ObjectDescriptor[] {});
+            fail("null name should have thrown a NullPointerException");
+        }
+        catch (NullPointerException npe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            new MetaData("name", null, new ObjectDescriptor[] {});
+            fail("null description should have thrown a NullPointerException");
+        }
+        catch (NullPointerException npe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            new MetaData("name", "description", (ObjectDescriptor[]) null);
+            fail("null objectDescriptors should have thrown a NullPointerException");
+        }
+        catch (NullPointerException npe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            new MetaData("name", "description", (ObjectDescriptor) null);
+            fail("null objectDescriptors should have thrown a NullPointerException");
+        }
+        catch (NullPointerException npe)
+        {
+            // Ignore expected exception
+        }
+
+        metaData = new MetaData("name", "description", new ObjectDescriptor("integer", "integers only please", Integer.class));
+        assertEquals("name", "name", metaData.getName());
+        assertEquals("description", "description", metaData.getDescription());
+        assertEquals("check name of object descriptor", "integer", metaData.getFieldName(0));
+        assertEquals("check description of object descriptor", "integers only please", metaData.getFieldDescription(0));
+        assertEquals("check class of object descriptor", Integer.class, metaData.getFieldClass(0));
+        assertEquals("size should be 0", 0, metaData.size());
+        try
+        {
+            metaData.getFieldName(-1);
+            fail("Bad index should have thrown an IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException ioobe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            metaData.getFieldName(1);
+            fail("Bad index should have thrown an IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException ioobe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            metaData.getFieldDescription(-1);
+            fail("Bad index should have thrown an IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException ioobe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            metaData.getFieldDescription(1);
+            fail("Bad index should have thrown an IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException ioobe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            metaData.getFieldClass(-1);
+            fail("Bad index should have thrown an IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException ioobe)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            metaData.getFieldClass(1);
+            fail("Bad index should have thrown an IndexOutOfBoundsException");
+        }
+        catch (IndexOutOfBoundsException ioobe)
+        {
+            // Ignore expected exception
+        }
+        
+        metaData = new MetaData("name", "description", new ObjectDescriptor[] {});
+        metaData.verifyComposition(null); // this null is allowed
+        metaData.verifyComposition(new Object[] {}); // empty Object array is too
+
+    }
+
+}
