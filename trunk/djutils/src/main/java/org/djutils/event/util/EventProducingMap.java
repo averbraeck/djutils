6e21efdf164248d24eee291847c74d6577f449f2
pknoppers <pknoppers@6bd9694c-b1be-467f-8798-70e1d16d5ec4>
2020-04-03 09:55:15 +0000

    Added MetaData argument to constructors of EventType. For now null is allowed, but this freedom will be removed soon.

--- trunk/djutils/src/main/java/org/djutils/event/util/EventProducingMap.java
+++ trunk/djutils/src/main/java/org/djutils/event/util/EventProducingMap.java
@@ -33,13 +33,13 @@ public class EventProducingMap<K, V> extends EventProducer implements Map<K, V>
     private static final long serialVersionUID = 20191230L;
 
     /** OBJECT_ADDED_EVENT is fired on new entries. */
-    public static final EventType OBJECT_ADDED_EVENT = new EventType("OBJECT_ADDED_EVENT");
+    public static final EventType OBJECT_ADDED_EVENT = new EventType("OBJECT_ADDED_EVENT", null);
 
     /** OBJECT_REMOVED_EVENT is fired on removel of entries. */
-    public static final EventType OBJECT_REMOVED_EVENT = new EventType("OBJECT_REMOVED_EVENT");
+    public static final EventType OBJECT_REMOVED_EVENT = new EventType("OBJECT_REMOVED_EVENT", null);
 
     /** OBJECT_CHANGED_EVENT is fired on change of one or more entries. */
-    public static final EventType OBJECT_CHANGED_EVENT = new EventType("OBJECT_CHANGED_EVENT");
+    public static final EventType OBJECT_CHANGED_EVENT = new EventType("OBJECT_CHANGED_EVENT", null);
 
     /** the parent map. */
     private final Map<K, V> parent;
