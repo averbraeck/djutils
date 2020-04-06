011444a63b61966bbc057b84f12999b597a5f050
OTSim <P.Knoppers@tudelft.nl>
2020-04-06 07:18:33 +0000

    All events now have MetaData.

--- djutils-stats/src/test/java/org/djutils/stats/summarizers/event/EventBasedWeightedTallyTest.java
+++ djutils-stats/src/test/java/org/djutils/stats/summarizers/event/EventBasedWeightedTallyTest.java
@@ -9,6 +9,8 @@ import org.djutils.event.Event;
 import org.djutils.event.EventInterface;
 import org.djutils.event.EventListenerInterface;
 import org.djutils.event.EventType;
+import org.djutils.metadata.MetaData;
+import org.djutils.metadata.ObjectDescriptor;
 import org.junit.Test;
 
 /**
@@ -25,7 +27,10 @@ import org.junit.Test;
 public class EventBasedWeightedTallyTest
 {
     /** an event to fire. */
-    private static final EventType VALUE_EVENT = new EventType("VALUE_EVENT", null);
+    private static final EventType VALUE_EVENT = new EventType("VALUE_EVENT",
+            new MetaData("WeightAndValue", "Double[] with Double weight and Double value ",
+                    new ObjectDescriptor[] { new ObjectDescriptor("Weight", "Double weight", Double.class),
+                            new ObjectDescriptor("Valuie", "Double value", Double.class) }));
 
     /** Test the EventBasedWeightedTally. */
     @Test
@@ -47,17 +52,17 @@ public class EventBasedWeightedTallyTest
         assertEquals(0.0, wt.getWeightedSum(), 0.0);
         assertEquals(0L, wt.getN());
 
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.1, 1.1}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.1, 1.2}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.1, 1.3}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.1, 1.4}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.1, 1.5}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.1, 1.6}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.1, 1.7}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.1, 1.8}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.1, 1.9}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.1, 2.0}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.1, 1.0}));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.1, 1.1 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.1, 1.2 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.1, 1.3 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.1, 1.4 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.1, 1.5 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.1, 1.6 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.1, 1.7 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.1, 1.8 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.1, 1.9 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.1, 2.0 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.1, 1.0 }));
 
         // Now we check the EventBasedWeightedTally
         assertEquals(2.0, wt.getMax(), 1.0E-6);
@@ -84,7 +89,7 @@ public class EventBasedWeightedTallyTest
 
         try
         {
-            wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {-0.1, 123.456}));
+            wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { -0.1, 123.456 }));
             fail("negative weight should have thrown an exception");
         }
         catch (IllegalArgumentException iae)
@@ -96,31 +101,31 @@ public class EventBasedWeightedTallyTest
             wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", "123"));
             fail("non Object[] content should have thrown an exception");
         }
-        catch (IllegalArgumentException iae)
+        catch (ClassCastException iae)
         {
             // Ignore expected exception
         }
         try
         {
-            wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.1}));
+            wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.1 }));
             fail("Object[] with one argument should have thrown an exception");
         }
-        catch (IllegalArgumentException iae)
+        catch (IndexOutOfBoundsException iae)
         {
             // Ignore expected exception
         }
         try
         {
-            wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.1, 0.2, 0.3}));
+            wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.1, 0.2, 0.3 }));
             fail("Object[] with thre arguments should have thrown an exception");
         }
-        catch (IllegalArgumentException iae)
+        catch (IndexOutOfBoundsException iae)
         {
             // Ignore expected exception
         }
         try
         {
-            wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {"bla", 0.2}));
+            wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { "bla", 0.2 }));
             fail("EventBasedWeightedTally should fail on weight !instanceOf Double");
         }
         catch (Exception exception)
@@ -129,7 +134,7 @@ public class EventBasedWeightedTallyTest
         }
         try
         {
-            wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.3, "bla"}));
+            wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.3, "bla" }));
             fail("EventBasedWeightedTally should fail on value !instanceOf Double");
         }
         catch (Exception exception)
@@ -146,9 +151,9 @@ public class EventBasedWeightedTallyTest
         // From: https://sciencing.com/calculate-time-decimals-5962681.html
         EventBasedWeightedTally wt = new EventBasedWeightedTally("simple EventBasedWeightedTally statistic");
         wt.initialize();
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {13.0, 86.0}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {23.0, 26.0}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {4.0, 0.0}));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 13.0, 86.0 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 23.0, 26.0 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 4.0, 0.0 }));
 
         assertEquals(1716.0, wt.getWeightedSum(), 0.001);
         assertEquals(42.9, wt.getWeightedSampleMean(), 0.001);
@@ -156,9 +161,9 @@ public class EventBasedWeightedTallyTest
         // When we shift the times, we should get the same answers
         wt = new EventBasedWeightedTally("simple EventBasedWeightedTally statistic");
         wt.initialize();
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {13.0, 86.0}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {23.0, 26.0}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {14.0, 0.0}));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 13.0, 86.0 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 23.0, 26.0 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 14.0, 0.0 }));
 
         assertEquals(1716.0, wt.getWeightedSum(), 0.001);
         assertEquals(34.32, wt.getWeightedSampleMean(), 0.001);
@@ -166,26 +171,26 @@ public class EventBasedWeightedTallyTest
         // When we have observations with duration 0, we should get the same answers
         wt = new EventBasedWeightedTally("simple EventBasedWeightedTally statistic");
         wt.initialize();
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {13.0, 86.0}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.0, 86.0}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {23.0, 26.0}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {4.0, 0.0}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0.0, 0.0}));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 13.0, 86.0 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.0, 86.0 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 23.0, 26.0 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 4.0, 0.0 }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0.0, 0.0 }));
 
         assertEquals(1716.0, wt.getWeightedSum(), 0.001);
         assertEquals(42.9, wt.getWeightedSampleMean(), 0.001);
 
         // Example from NIST: https://www.itl.nist.gov/div898/software/dataplot/refman2/ch2/weightsd.pdf
         wt = new EventBasedWeightedTally("NIST");
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {1, 2}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {1, 3}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0, 5}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0, 7}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {4, 11}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {1, 13}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {2, 17}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {1, 19}));
-        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] {0, 23}));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 1d, 2d }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 1d, 3d }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0d, 5d }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0d, 7d }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 4d, 11d }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 1d, 13d }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 2d, 17d }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 1d, 19d }));
+        wt.notify(new Event(VALUE_EVENT, "EventBasedWeightedTallyTest", new Object[] { 0d, 23d }));
 
         assertEquals((2 + 3 + 4 * 11 + 13 + 2 * 17 + 19) / 10.0, wt.getWeightedSampleMean(), 0.001);
         assertEquals(5.82, wt.getWeightedSampleStDev(), 0.01);
@@ -203,11 +208,11 @@ public class EventBasedWeightedTallyTest
         weightedTally.addListener(woel, StatisticsEvents.WEIGHTED_OBSERVATION_ADDED_EVENT);
         assertEquals(0, woel.getObservationEvents());
 
-        EventType[] types = new EventType[] {StatisticsEvents.N_EVENT, StatisticsEvents.MIN_EVENT, StatisticsEvents.MAX_EVENT,
+        EventType[] types = new EventType[] { StatisticsEvents.N_EVENT, StatisticsEvents.MIN_EVENT, StatisticsEvents.MAX_EVENT,
                 StatisticsEvents.WEIGHTED_POPULATION_MEAN_EVENT, StatisticsEvents.WEIGHTED_POPULATION_VARIANCE_EVENT,
                 StatisticsEvents.WEIGHTED_POPULATION_STDEV_EVENT, StatisticsEvents.WEIGHTED_SUM_EVENT,
                 StatisticsEvents.WEIGHTED_SAMPLE_MEAN_EVENT, StatisticsEvents.WEIGHTED_SAMPLE_VARIANCE_EVENT,
-                StatisticsEvents.WEIGHTED_SAMPLE_STDEV_EVENT};
+                StatisticsEvents.WEIGHTED_SAMPLE_STDEV_EVENT };
         LoggingEventListener[] listeners = new LoggingEventListener[types.length];
         for (int i = 0; i < types.length; i++)
         {
@@ -223,8 +228,7 @@ public class EventBasedWeightedTallyTest
         assertEquals(10, woel.getObservationEvents());
 
         // values based on formulas from https://www.itl.nist.gov/div898/software/dataplot/refman2/ch2/weightsd.pdf
-        Object[] expectedValues =
-                new Object[] {10L, 10.0, 100.0, 70.0, 600.0, 24.4949, 3850.0, 70.0, 666.6667, 25.81989};
+        Object[] expectedValues = new Object[] { 10L, 10.0, 100.0, 70.0, 600.0, 24.4949, 3850.0, 70.0, 666.6667, 25.81989 };
         for (int i = 0; i < types.length; i++)
         {
             assertEquals("Number of events for listener " + types[i], 10, listeners[i].getNumberOfEvents());
