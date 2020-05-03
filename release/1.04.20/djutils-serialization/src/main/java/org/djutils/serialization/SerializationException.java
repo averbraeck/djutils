b906a8c1fd52d48dbd9d579675c75d67676d5fab
averbraeck <averbraeck@6bd9694c-b1be-467f-8798-70e1d16d5ec4>
2020-05-03 17:03:58 +0000

    deployed version 1.04.20

---
+++ release/1.04.20/djutils-serialization/src/main/java/org/djutils/serialization/SerializationException.java
@@ -0,0 +1,67 @@
+package org.djutils.serialization;
+
+/**
+ * Exception for the DSOL ZeroMQ bridge.
+ * <p>
+ * Copyright (c) 2019-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://opentrafficsim.org/node/13">OpenTrafficSim License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, <br>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="https://www.tudelft.nl/staff/p.knoppers/">Peter Knoppers</a>
+ * @author <a href="https://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ */
+public class SerializationException extends Exception
+{
+    /** */
+    private static final long serialVersionUID = 20190611L;
+
+    /**
+     * Create a serialization exception.
+     */
+    public SerializationException()
+    {
+    }
+
+    /**
+     * Create a serialization exception.
+     * @param message the message
+     */
+    public SerializationException(final String message)
+    {
+        super(message);
+    }
+
+    /**
+     * Create a serialization exception.
+     * @param cause the exception that caused the serialization exception
+     */
+    public SerializationException(final Throwable cause)
+    {
+        super(cause);
+    }
+
+    /**
+     * Create a serialization exception.
+     * @param message the message
+     * @param cause the exception that caused the serialization exception
+     */
+    public SerializationException(final String message, final Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    /**
+     * Create a serialization exception.
+     * @param message the message
+     * @param cause the exception that caused the serialization exception
+     * @param enableSuppression to enable suppressions or not
+     * @param writableStackTrace to have a writable stack trace or not
+     */
+    public SerializationException(final String message, final Throwable cause, final boolean enableSuppression,
+            final boolean writableStackTrace)
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
