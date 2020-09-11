4ccf94b9daea08f7a2537b546bba07df7eae7ba3
averbraeck <averbraeck@6bd9694c-b1be-467f-8798-70e1d16d5ec4>
2020-09-11 19:46:08 +0000

    Improved the javadoc with the right license file and project (some pointed still to dsol or opentrafficsim).

--- trunk/djutils-draw/src/main/java/org/djutils/draw/d2/Area2d.java
+++ trunk/djutils-draw/src/main/java/org/djutils/draw/d2/Area2d.java
@@ -4,7 +4,7 @@ package org.djutils.draw.d2;
  * Area2d.java.
  * <p>
  * Copyright (c) 2020-2020 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
- * BSD-style license. See <a href="https://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
+ * BSD-style license. See <a href="https://djutils.org/docs/current/djutils/licenses.html">DJUTILS License</a>.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  * @author <a href="https://www.tudelft.nl/pknoppers">Peter Knoppers</a>
