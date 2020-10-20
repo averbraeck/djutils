eef711f355ceae1a78a61c40dc66112b9d0e0d63
OTSim <P.Knoppers@tudelft.nl>
2020-10-20 06:38:58 +0000

    Major overhaul.

--- djutils/src/main/java/org/djutils/quadtree/Envelope.java
+++ djutils/src/main/java/org/djutils/quadtree/Envelope.java
@@ -5,7 +5,7 @@ package org.djutils.quadtree;
  * <p>
  * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
  */
-public interface BoundingBoxed
+public interface Envelope
 {
     /**
      * Determine the bounding box. The result must be constant; it may not vary from one call to the next. Objects implementing
@@ -13,12 +13,5 @@ public interface BoundingBoxed
      * @return Rectangle; the bounding box
      */
     Rectangle getBoundingRectangle();
-
-    /**
-     * Determine if this 2D object intersects the given rectangle.
-     * @param rectangle Rectangle; the rectangle
-     * @return boolean; true if this object intersects the rectangle; false if the object does not intersect the rectangle
-     */
-    boolean intersects(Rectangle rectangle);
 
 }
