a74020b59cb5cedc2600e9de0c48bb6960fd490e
averbraeck <averbraeck@6bd9694c-b1be-467f-8798-70e1d16d5ec4>
2020-12-22 15:35:35 +0000

    Deployed version 1.06.02 of djutils

---
+++ release/1.06.02/djutils/src/main/java/org/djutils/event/TimedEventInterface.java
@@ -0,0 +1,24 @@
+package org.djutils.event;
+
+import java.io.Serializable;
+
+/**
+ * The TimedEventInterface defines the getTimeStamp method on top of the methods that a regular event has to implement. <br>
+ * <br>
+ * Copyright (c) 2020-2020 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>. <br>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="https://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @param <T> the Comparable type that represents time
+ */
+public interface TimedEventInterface<T extends Comparable<T> & Serializable> extends EventInterface
+{
+    /**
+     * Returns the timeStamp of this event.
+     * @return T; the time stamp
+     */
+    T getTimeStamp();
+
+}
