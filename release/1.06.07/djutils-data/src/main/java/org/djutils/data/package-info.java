3693858580c7df6cc0db0ddca84c38145d6740ae
averbraeck <averbraeck@6bd9694c-b1be-467f-8798-70e1d16d5ec4>
2021-01-23 12:17:01 +0000

    Deployed version 1.06.07 of djutils

---
+++ release/1.06.07/djutils-data/src/main/java/org/djutils/data/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Data collection utilities. The main data storage can be seen as a table with records and column headings.
+ * <p>
+ * Copyright (c) 2020-2021 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://djutils.org/docs/current/djutils/licenses.html">DJUTILS License</a>.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="https://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+package org.djutils.data;
