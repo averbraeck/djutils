583206502cb1557fceb5eca1c46273260514afb4
pknoppers <pknoppers@6bd9694c-b1be-467f-8798-70e1d16d5ec4>
2021-04-13 19:52:56 +0000

    Added two java implementations of hypot (which do not quite conform to our code style). Both are about 7 times faster than Math.hypot. Accuracy is not checked.

--- trunk/djutils/src/test/java/org/djutils/complex/demo/PerformanceTests.java
+++ trunk/djutils/src/test/java/org/djutils/complex/demo/PerformanceTests.java
@@ -59,6 +59,28 @@ public final class PerformanceTests
         startNanos = System.nanoTime();
         for (int i = 0; i < iterations; i++)
         {
+            double x = 0.1 + i / 1000.0;
+            double y = -0.5 + i / 2000.0;
+            PerformanceTests.hypotA(y, x);
+        }
+        nowNanos = System.nanoTime();
+        durationNanos = nowNanos - startNanos;
+        System.out.println(String.format("PerformanceTests.hypotA: %d invocations in %.6f s (%.1f ns/invocation)", iterations,
+                durationNanos / 1000000000.0, 1.0 * durationNanos / iterations));
+        startNanos = System.nanoTime();
+        for (int i = 0; i < iterations; i++)
+        {
+            double x = 0.1 + i / 1000.0;
+            double y = -0.5 + i / 2000.0;
+            PerformanceTests.hypotB(y, x);
+        }
+        nowNanos = System.nanoTime();
+        durationNanos = nowNanos - startNanos;
+        System.out.println(String.format("PerformanceTests.hypotB: %d invocations in %.6f s (%.1f ns/invocation)", iterations,
+                durationNanos / 1000000000.0, 1.0 * durationNanos / iterations));
+        startNanos = System.nanoTime();
+        for (int i = 0; i < iterations; i++)
+        {
             double x = 0.1 + i / 1000000.0;
             Math.sin(x);
         }
@@ -100,4 +122,146 @@ public final class PerformanceTests
                 durationNanos / 1000000000.0, 1.0 * durationNanos / iterations));
     }
 
+    /** 2^450. */
+    private static final double TWO_POW_450 = Double.longBitsToDouble(0x5C10000000000000L);
+
+    /** 2^-450. */
+    private static final double TWO_POW_N450 = Double.longBitsToDouble(0x23D0000000000000L);
+
+    /** 2^750? */
+    private static final double TWO_POW_750 = Double.longBitsToDouble(0x6ED0000000000000L);
+
+    /** 2^-750? */
+    private static final double TWO_POW_N750 = Double.longBitsToDouble(0x1110000000000000L);
+
+    /**
+     * Implementation of hypot taken from https://stackoverflow.com/questions/3764978/why-hypot-function-is-so-slow
+     * @param x double; x
+     * @param y double; y
+     * @return double; the hypot of x, and y
+     */
+    public static double hypotA(double x, double y)
+    {
+        x = Math.abs(x);
+        y = Math.abs(y);
+        if (y < x)
+        {
+            double a = x;
+            x = y;
+            y = a;
+        }
+        else if (!(y >= x))
+        { // Testing if we have some NaN.
+            if ((x == Double.POSITIVE_INFINITY) || (y == Double.POSITIVE_INFINITY))
+            {
+                return Double.POSITIVE_INFINITY;
+            }
+            else
+            {
+                return Double.NaN;
+            }
+        }
+        if (y - x == y)
+        { // x too small to substract from y
+            return y;
+        }
+        else
+        {
+            double factor;
+            if (x > TWO_POW_450)
+            { // 2^450 < x < y
+                x *= TWO_POW_N750;
+                y *= TWO_POW_N750;
+                factor = TWO_POW_750;
+            }
+            else if (y < TWO_POW_N450)
+            { // x < y < 2^-450
+                x *= TWO_POW_750;
+                y *= TWO_POW_750;
+                factor = TWO_POW_N750;
+            }
+            else
+            {
+                factor = 1.0;
+            }
+            return factor * Math.sqrt(x * x + y * y);
+        }
+    }
+
+    /**
+     * <b>hypot</b>.
+     * @param x double; x
+     * @param y double; y
+     * @return sqrt(x*x +y*y) without intermediate overflow or underflow.
+     * @Note {@link Math#hypot} is unnecessarily slow. This returns the identical result to Math.hypot with reasonable run times
+     *       (~40 nsec vs. 800 nsec).
+     *       <p>
+     *       The logic for computing z is copied from "Freely Distributable Math Library" fdlibm's e_hypot.c. This minimizes
+     *       rounding error to provide 1 ulb accuracy.
+     */
+    public static double hypotB(double x, double y)
+    {
+        if (Double.isInfinite(x) || Double.isInfinite(y))
+        {
+            return Double.POSITIVE_INFINITY;
+        }
+        if (Double.isNaN(x) || Double.isNaN(y))
+        {
+            return Double.NaN;
+        }
+
+        x = Math.abs(x);
+        y = Math.abs(y);
+
+        if (x < y)
+        {
+            double d = x;
+            x = y;
+            y = d;
+        }
+
+        int xi = Math.getExponent(x);
+        int yi = Math.getExponent(y);
+
+        if (xi > yi + 27)
+        {
+            return x;
+        }
+
+        int bias = 0;
+        if (xi > 510 || xi < -511)
+        {
+            bias = xi;
+            x = Math.scalb(x, -bias);
+            y = Math.scalb(y, -bias);
+        }
+
+        // translated from "Freely Distributable Math Library" e_hypot.c to minimize rounding errors
+        double z = 0;
+        if (x > 2 * y)
+        {
+            double x1 = Double.longBitsToDouble(Double.doubleToLongBits(x) & 0xffffffff00000000L);
+            double x2 = x - x1;
+            z = Math.sqrt(x1 * x1 + (y * y + x2 * (x + x1)));
+        }
+        else
+        {
+            double t = 2 * x;
+            double t1 = Double.longBitsToDouble(Double.doubleToLongBits(t) & 0xffffffff00000000L);
+            double t2 = t - t1;
+            double y1 = Double.longBitsToDouble(Double.doubleToLongBits(y) & 0xffffffff00000000L);
+            double y2 = y - y1;
+            double xMinusY = x - y;
+            z = Math.sqrt(t1 * y1 + (xMinusY * xMinusY + (t1 * y2 + t2 * y))); // Note: 2*x*y <= x*x + y*y
+        }
+
+        if (bias == 0)
+        {
+            return z;
+        }
+        else
+        {
+            return Math.scalb(z, bias);
+        }
+    }
 }
