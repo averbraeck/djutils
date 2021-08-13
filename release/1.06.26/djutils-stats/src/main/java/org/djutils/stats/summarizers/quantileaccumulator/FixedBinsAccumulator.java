74e8de210fefe661c71c1249dc0489cd6c0dd1e3
averbraeck <averbraeck@6bd9694c-b1be-467f-8798-70e1d16d5ec4>
2021-08-13 22:28:36 +0000

    deployed version 1.06.26

---
+++ release/1.06.26/djutils-stats/src/main/java/org/djutils/stats/summarizers/quantileaccumulator/FixedBinsAccumulator.java
@@ -0,0 +1,195 @@
+package org.djutils.stats.summarizers.quantileaccumulator;
+
+import org.djutils.exceptions.Throw;
+import org.djutils.stats.summarizers.Tally;
+
+/**
+ * FixedBinsAccumulator.java. <br>
+ * This accumulator is created with a caller prescribes set of bins. All bins have the same width.
+ * <br>
+ * Copyright (c) 2021-2021 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>. <br>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="https://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class FixedBinsAccumulator implements QuantileAccumulator
+{
+    /** Center value of minimum bin. */
+    private final double minimumBinCenter;
+
+    /** Width of each bin. */
+    private final double binWidth;
+
+    /** Storage for the accumulated values. */
+    private long[] accumulator;
+
+    /** Cumulative counts. */
+    private long[] cumulatives = null;
+
+    /** Total number of ingested values. */
+    private long totalCount = 0;
+
+    /** Count number of ingested items that fall below the range. */
+    private long belowCount = 0;
+
+    /** Count number of ingested items that fall above the range. */
+    private long aboveCount = 0;
+
+    /**
+     * Construct a new FullStorageAccumulator.
+     * @param minimumBinCenter double; center value of bin for minimum value of range (minimum value in range is
+     *            <code>minimumBinCenter - binWidth / 2</code>, maximum value in range is
+     *            <code>minimumBinCenter + binWidth * (binCount - 0.5)</code>)
+     * @param binWidth double; width of each bin
+     * @param binCount int; number of bins
+     */
+    public FixedBinsAccumulator(final double minimumBinCenter, final double binWidth, final int binCount)
+    {
+        Throw.when(!Double.isFinite(minimumBinCenter), IllegalArgumentException.class, "minimumBinCenter must be finite");
+        Throw.when(!Double.isFinite(binWidth), IllegalArgumentException.class, "binWidth must be finite");
+        Throw.when(binWidth <= 0, IllegalArgumentException.class, "binWidth must be positive");
+        Throw.when(binCount < 1, IllegalArgumentException.class, "binCount must be > 0");
+        this.minimumBinCenter = minimumBinCenter;
+        this.binWidth = binWidth;
+        this.belowCount = 0;
+        this.aboveCount = 0;
+        this.accumulator = new long[binCount];
+        this.cumulatives = null;
+        this.totalCount = 0;
+    }
+
+    /**
+     * Retrieve the bin width.
+     * @return double; the bin width
+     */
+    public double getBinWidth()
+    {
+        return this.binWidth;
+    }
+
+    /**
+     * Retrieve the bin count.
+     * @return int; the bin count
+     */
+    public int getBinCount()
+    {
+        return this.accumulator.length;
+    }
+
+    /**
+     * Retrieve the total number of ingested values.
+     * @return long; the total number of ingested values
+     */
+    public long getN()
+    {
+        return this.totalCount;
+    }
+
+    /**
+     * Retrieve the number of ingested values that were below the range of this FixedBinsAccumulator.
+     * @return long; the number of ingested values that were below the range of this FixedBinsAccumulator
+     */
+    public long getBelowCount()
+    {
+        return this.belowCount;
+    }
+
+    /**
+     * Retrieve the number of ingested values that were above the range of this FixedBinsAccumulator.
+     * @return long; the number of ingested values that were above the range of this FixedBinsAccumulator
+     */
+    public long getAboveCount()
+    {
+        return this.aboveCount;
+    }
+
+    /**
+     * Return the center of a particular bin.
+     * @param bin int the bin number
+     * @return double; the center of requested bin
+     */
+    public double getBinCenter(final int bin)
+    {
+        Throw.when(bin < 0 || bin >= this.accumulator.length, IllegalArgumentException.class,
+                "bin must be in range 0..$1; got $2", this.accumulator.length - 1, bin);
+        return this.minimumBinCenter + bin * this.binWidth;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public double ingest(final double value)
+    {
+        Throw.when(Double.isNaN(value), IllegalArgumentException.class, "accumulator can not accumlate NaN value");
+        int bin = (int) ((value - this.minimumBinCenter) / binWidth);
+        if (bin < 0)
+        {
+            this.belowCount++;
+        }
+        else if (bin >= this.accumulator.length)
+        {
+            this.aboveCount++;
+        }
+        else
+        {
+            this.accumulator[bin]++;
+        }
+        this.totalCount++;
+        return value;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public double getQuantile(final Tally tally, final double probability)
+    {
+        Throw.when(!Double.isFinite(probability) || probability < 0.0 || probability > 1.0, IllegalArgumentException.class,
+                "probability must be a value between 0 and 1");
+        if (null == this.cumulatives)
+        {
+            long count = 0;
+            this.cumulatives = new long[this.accumulator.length];
+            for (int bin = 0; bin < this.accumulator.length; bin++)
+            {
+                count += this.accumulator[bin];
+                this.cumulatives[bin] = count;
+            }
+        }
+        // TODO do something clever with belowCount and aboveCount (could involve the tally as well)
+        long usableCount = this.totalCount - this.belowCount - this.aboveCount;
+        if (usableCount == 0)
+        {
+            return Double.NaN;
+        }
+        double value = (usableCount) * probability;
+        // TODO use bisection to home in
+        for (int bin = 0; bin < this.cumulatives.length; bin++)
+        {
+            if (this.cumulatives[bin] >= value)
+            {
+                return bin * this.binWidth + this.minimumBinCenter;
+            }
+        }
+        return 0; // cannot happen
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void initialize()
+    {
+        this.belowCount = 0;
+        this.aboveCount = 0;
+        this.accumulator = new long[this.accumulator.length];
+        this.cumulatives = null;
+        this.totalCount = 0;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String toString()
+    {
+        return "FixedBinsAccumulator [minimumBinCenter=" + minimumBinCenter + ", binWidth=" + binWidth + ", totalCount="
+                + totalCount + ", belowCount=" + belowCount + ", aboveCount=" + aboveCount + "]";
+    }
+
+}
---
+++ release/1.06.26/djutils-stats/src/test/java/org/djutils/stats/summarizers/quantileaccumulator/FixedBinsAccumulatorTest.java
@@ -0,0 +1,232 @@
+package org.djutils.stats.summarizers.quantileaccumulator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * FixedBinsAccumulatorTest.java. <br>
+ * <br>
+ * Copyright (c) 2021-2021 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>. <br>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="https://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class FixedBinsAccumulatorTest
+{
+    /**
+     * Test the FixedBinsAccumulator.
+     */
+    @Test
+    public void testFixedBinsAccumulator()
+    {
+        try
+        {
+            new FixedBinsAccumulator(Double.NaN, 0.25, 100);
+            fail("Illegal minimumBinCenter should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            new FixedBinsAccumulator(Double.NEGATIVE_INFINITY, 0.25, 100);
+            fail("Illegal minimumBinCenter should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            new FixedBinsAccumulator(Double.POSITIVE_INFINITY, 0.25, 100);
+            fail("Illegal minimumBinCenter should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            new FixedBinsAccumulator(0.0, Double.NaN, 100);
+            fail("Illegal binWidth should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            new FixedBinsAccumulator(0.0, Double.POSITIVE_INFINITY, 100);
+            fail("Illegal binWidth should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            new FixedBinsAccumulator(0.0, Double.NEGATIVE_INFINITY, 100);
+            fail("Illegal binWidth should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            new FixedBinsAccumulator(0.0, 0.0, 100);
+            fail("Illegal binWidth should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            new FixedBinsAccumulator(0.0, 0.25, 0);
+            fail("Illegal binCount should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+
+        FixedBinsAccumulator fba = new FixedBinsAccumulator(10.0, 0.25, 100);
+        assertEquals("binWidth", 0.25, fba.getBinWidth(), 0.0);
+        assertEquals("bin count", 100, fba.getBinCount());
+        assertEquals("total count", 0, fba.getN());
+        assertEquals("below count", 0, fba.getBelowCount());
+        assertEquals("above count", 0, fba.getAboveCount());
+        assertTrue("quantile cannot be computed yet", Double.isNaN(fba.getQuantile(null, 0.5)));
+
+        for (int bin = 0; bin < 100; bin++)
+        {
+            assertEquals("Bin center", 10.0 + 0.25 * bin, fba.getBinCenter(bin), 0.0001);
+        }
+        try
+        {
+            fba.getBinCenter(-1);
+            fail("negative bin number should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+
+        try
+        {
+            fba.getBinCenter(100);
+            fail("Too large bin number should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+
+        fba = new FixedBinsAccumulator(1, (Math.E - 1) / 1000, 1001);
+        assertEquals("highest bin is near e", Math.E, fba.getBinCenter(fba.getBinCount() - 1), 0.00001);
+        // ingest some values with an exponential density distribution
+        int max = 1000000;
+        for (int i = 0; i < max; i++)
+        {
+            double range0to1 = 1.0 * i / max;
+            double value = Math.exp(range0to1); // Should be in range 0..e
+            fba.ingest(value);
+        }
+        assertEquals("total values is max", max, fba.getN());
+        assertEquals("no values below 1", 0, fba.getBelowCount());
+        assertEquals("no values above e", 0, fba.getAboveCount());
+        assertEquals("0% quantile", 1.0, fba.getQuantile(null, 0.0), 0.001);
+        assertEquals("100% quantile", Math.E, fba.getQuantile(null, 1.0), Math.E / 1000);
+        assertEquals("50% quantile", Math.sqrt(Math.E), fba.getQuantile(null, 0.5), Math.E / 1000);
+        assertEquals("25% quantile", Math.sqrt(Math.sqrt(Math.E)), fba.getQuantile(null, 0.25), Math.E / 1000);
+        
+        // Check the below and above counters
+        for (int i = 0; i < 5; i++)
+        {
+            fba.ingest(-i);
+            assertEquals("below counter", i + 1, fba.getBelowCount());
+            assertEquals("above counter", 0, fba.getAboveCount());
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            fba.ingest(10);
+            assertEquals("below counter", 5, fba.getBelowCount());
+            assertEquals("above counter", i + 1, fba.getAboveCount());
+        }
+        
+        try
+        {
+            fba.getQuantile(null, -0.001);
+            fail("Negative probability should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+        
+        try
+        {
+            fba.getQuantile(null, 1.001);
+            fail("Negative probability should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+        
+        try
+        {
+            fba.getQuantile(null, Double.POSITIVE_INFINITY);
+            fail("Negative probability should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+        
+        try
+        {
+            fba.getQuantile(null, Double.NEGATIVE_INFINITY);
+            fail("Negative probability should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+        
+        try
+        {
+            fba.getQuantile(null, Double.NaN);
+            fail("Negative probability should have thrown an IllegalArgumentException");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Ignore expected exception
+        }
+        
+        fba.initialize();
+        assertEquals("binWidth", (Math.E - 1) / 1001, fba.getBinWidth(), 0.00001);
+        assertEquals("bin count", 1001, fba.getBinCount());
+        assertEquals("total count", 0, fba.getN());
+        assertEquals("below count", 0, fba.getBelowCount());
+        assertEquals("above count", 0, fba.getAboveCount());
+        assertTrue("quantile cannot be computed yet", Double.isNaN(fba.getQuantile(null, 0.5)));
+        
+        assertTrue("toString returns something descriptive", fba.toString().startsWith("FixedBinsAccumulator "));
+    }
+
+}
