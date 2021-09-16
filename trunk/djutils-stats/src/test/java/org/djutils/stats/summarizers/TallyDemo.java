c051e5302bb2d5f5c08adf4edfc2d1e354d074b9
pknoppers <pknoppers@6bd9694c-b1be-467f-8798-70e1d16d5ec4>
2021-09-16 16:48:30 +0000

    Expanded demo code.

--- trunk/djutils-stats/src/test/java/org/djutils/stats/summarizers/TallyDemo.java
+++ trunk/djutils-stats/src/test/java/org/djutils/stats/summarizers/TallyDemo.java
@@ -55,8 +55,13 @@ public final class TallyDemo
         System.out.println("first quartile:                " + tally.getQuantile(0.25));
         System.out.println("median:                        " + tally.getQuantile(0.5));
         System.out.println("third quartile:                " + tally.getQuantile(0.75));
+        for (int bin = 0; bin <= 10; bin++)
+        {
+            double value = bin / 10.0;
+            System.out.println("Cumulative probability at " + value + "  " + tally.getCumulativeProbability(value));
+        }
 
-        System.out.println();
+        System.out.println("End of NoStorageAccumulator output.\n");
         tally = new Tally("Example tally with full storage accumulator", new FullStorageAccumulator());
         random = new Random(1234);
         for (int i = 0; i < 1000; i++)
@@ -82,8 +87,13 @@ public final class TallyDemo
         System.out.println("first quartile:                " + tally.getQuantile(0.25));
         System.out.println("median:                        " + tally.getQuantile(0.5));
         System.out.println("third quartile:                " + tally.getQuantile(0.75));
+        for (int bin = 0; bin <= 10; bin++)
+        {
+            double value = bin / 10.0;
+            System.out.println("Cumulative probability at " + value + "  " + tally.getCumulativeProbability(value));
+        }
 
-        System.out.println();
+        System.out.println("End of FullStorageAccumulator output.\n");
         tally = new Tally("Example tally with TDigest accumulator", new TDigestAccumulator());
         random = new Random(1234);
         for (int i = 0; i < 1000; i++)
@@ -109,6 +119,11 @@ public final class TallyDemo
         System.out.println("first quartile:                " + tally.getQuantile(0.25));
         System.out.println("median:                        " + tally.getQuantile(0.5));
         System.out.println("third quartile:                " + tally.getQuantile(0.75));
+        for (int bin = 0; bin <= 10; bin++)
+        {
+            double value = bin / 10.0;
+            System.out.println("Cumulative probability at " + value + "  " + tally.getCumulativeProbability(value));
+        }
 
         System.out.println();
         tally = new Tally("Example tally with TDigest accumulator with higher precision", new TDigestAccumulator(1000));
@@ -136,9 +151,34 @@ public final class TallyDemo
         System.out.println("first quartile:                " + tally.getQuantile(0.25));
         System.out.println("median:                        " + tally.getQuantile(0.5));
         System.out.println("third quartile:                " + tally.getQuantile(0.75));
+        for (int bin = 0; bin <= 10; bin++)
+        {
+            double value = bin / 10.0;
+            System.out.println("Cumulative probability at " + value + "  " + tally.getCumulativeProbability(value));
+        }
 
-        System.out.println();
-        
+        System.out.println("End of TDigestAccumulator output.\n");
+
+        tally = new Tally("Example tally with FixedBinsAccumulator using 1000 bins",
+                new FixedBinsAccumulator(0.0005, 0.001, 1000));
+        random = new Random(1234);
+        for (int i = 0; i < 1000000; i++)
+        {
+            double d = random.nextDouble();
+            tally.ingest(d);
+        }
+        System.out.println("0% quantile (should be close to 0.0):            " + tally.getQuantile(0.0));
+        System.out.println("25% quantile (should be close to 0.25):          " + tally.getQuantile(0.25));
+        System.out.println("50% quantile (should be close to 0.5):           " + tally.getQuantile(0.50));
+        System.out.println("100% quantile (should be close to 1.0):          " + tally.getQuantile(1.0));
+        for (int bin = 0; bin < 10; bin++) // only print the first 10 bin counts
+        {
+            System.out.println("Number of values in bin " + bin + "                        " + (int) Math.rint(tally.getN()
+                    * (tally.getCumulativeProbability((bin + 1) / 1000.0) - tally.getCumulativeProbability(bin / 1000.0))));
+        }
+
+        System.out.println("");
+
         tally = new Tally("Example tally with FixedBinsAccumulator using 1001 bins",
                 new FixedBinsAccumulator(1.0, (Math.E - 1.0) / 1000, 1001));
         // Feed the tally perfectly exponentially distributed values in the interval [1.0,e)
@@ -151,7 +191,14 @@ public final class TallyDemo
         System.out.println("25% quantile (should be close to sqrt(sqrt(e))): " + tally.getQuantile(0.25));
         System.out.println("50% quantile (should be close to sqrt(e)):       " + tally.getQuantile(0.50));
         System.out.println("100% quantile (should be close to e):            " + tally.getQuantile(1.0));
+        for (int bin = 0; bin < 10; bin++)
+        {
+            double value = 1 + bin / 10.0 * (Math.E - 1);
+            System.out.println(String.format("Cumulative probability at %8f               ", value)
+                    + tally.getCumulativeProbability(value));
+        }
 
+        System.out.println("End of FixedBinsAccumulator output.\n");
     }
 
 }
