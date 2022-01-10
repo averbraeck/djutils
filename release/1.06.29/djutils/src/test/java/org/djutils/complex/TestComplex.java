5278d867a17535dd00e873ea3d286504603479c3
averbraeck <averbraeck@6bd9694c-b1be-467f-8798-70e1d16d5ec4>
2022-01-10 13:53:41 +0000

    deployed version 1.06.29

---
+++ release/1.06.29/djutils/src/test/java/org/djutils/complex/TestComplex.java
@@ -0,0 +1,225 @@
+package org.djutils.complex;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.djutils.base.AngleUtil;
+import org.junit.Test;
+
+/**
+ * TestComplex.java. <br>
+ * <br>
+ * Copyright (c) 2021-2022 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>. <br>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="https://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public class TestComplex
+{
+
+    /**
+     * Test the various constructors of Complex.
+     */
+    @Test
+    public void testConstructors()
+    {
+        double[] testValues = new double[] { 0, 1, 100, -1, -10000, Math.PI };
+
+        for (double re : testValues)
+        {
+            for (double im : testValues)
+            {
+                Complex complex = new Complex(re, im);
+                assertEquals("re", re, complex.re, 0.0001);
+                assertEquals("im", im, complex.im, 0.0001);
+                assertEquals("getRe", re, complex.getRe(), 0.0001);
+                assertEquals("getIm", im, complex.getIm(), 0.0001);
+                assertEquals("norm", Math.hypot(re, im), complex.norm(), 0.0001);
+                if (re != 0 || im != 0)
+                {
+                    assertEquals("phi", Math.atan2(im, re), complex.phi(), 0.000001);
+                }
+                if (im == 0)
+                {
+                    assertTrue("If imaginary part is 0; complex is pure real", complex.isReal());
+                    complex = new Complex(re);
+                    assertEquals("re", re, complex.re, 0.0001);
+                    assertEquals("im", im, complex.im, 0.0001);
+                    assertEquals("norm", Math.hypot(re, im), complex.norm(), 0.0001);
+                    if (re != 0)
+                    {
+                        assertEquals("phi", Math.atan2(im, re), complex.phi(), 0.000001);
+                    }
+                }
+                else
+                {
+                    assertFalse("If imaginary part is not null; complex is not pure real", complex.isReal());
+                }
+                if (re == 0)
+                {
+                    assertTrue("If real part is 0; complex is imaginary", complex.isImaginary());
+                }
+                else
+                {
+                    assertFalse("If real part is not 0; comples is not imaginary", complex.isImaginary());
+                }
+                Complex conjugate = complex.conjugate(); // Loss less operation; we can test for exact equality
+                assertEquals("Conjugate re", complex.re, conjugate.re, 0);
+                assertEquals("Conjugate im", -complex.im, conjugate.im, 0);
+            }
+        }
+    }
+
+    /**
+     * Test the constants defined by the Complex class.
+     */
+    @Test
+    public void testConstants()
+    {
+        assertEquals("real component of ZERO", 0, Complex.ZERO.re, 0);
+        assertEquals("imaginary component of ZERO", 0, Complex.ZERO.im, 0);
+        assertEquals("real component of ONE", 1, Complex.ONE.re, 0);
+        assertEquals("imaginary component of ONE", 0, Complex.ONE.im, 0);
+        assertEquals("real component of MINUS_ONE", -1, Complex.MINUS_ONE.re, 0);
+        assertEquals("imaginary component of MINUS_ONE", 0, Complex.MINUS_ONE.im, 0);
+        assertEquals("real component of I", 0, Complex.I.re, 0);
+        assertEquals("imaginary component of I", 1, Complex.I.im, 0);
+        assertEquals("real component of MINUS_I", 0, Complex.MINUS_I.re, 0);
+        assertEquals("imaginary component of MINUS_I", -1, Complex.MINUS_I.im, 0);
+    }
+
+    /**
+     * Test the methods.
+     */
+    @Test
+    public void testOperations()
+    {
+        Complex a = new Complex(12, -34);
+        Complex b = new Complex(-23, 45);
+        Complex c = a.plus(b);
+        assertEquals("sum re", a.re + b.re, c.re, 0.00001);
+        assertEquals("sum im", a.im + b.im, c.im, 0.00001);
+        c = a.plus(123);
+        assertEquals("sum re", a.re + 123, c.re, 0.00001);
+        assertEquals("sum im", a.im, c.im, 0.00001);
+        c = a.minus(b);
+        assertEquals("difference re", a.re - b.re, c.re, 0.00001);
+        assertEquals("difference im", a.im - b.im, c.im, 0.00001);
+        c = a.minus(123);
+        assertEquals("difference re", a.re - 123, c.re, 0.00001);
+        assertEquals("difference im", a.im, c.im, 0.00001);
+        c = a.times(b);
+        assertEquals("product norm", a.norm() * b.norm(), c.norm(), 0.0001);
+        assertEquals("product phi", a.phi() + b.phi(), c.phi(), 0.000001);
+        c = a.times(123);
+        assertEquals("product norm", a.norm() * 123, c.norm(), 0.0001);
+        assertEquals("product phi", a.phi(), c.phi(), 0.000001);
+        c = a.reciprocal();
+        assertEquals("norm of reciprocal", a.norm(), 1 / c.norm(), 0.00001);
+        assertEquals("phi of reciprocal", -a.phi(), c.phi(), 0.000001);
+        c = a.times(c);
+        assertEquals("a * a.reciprocal re", 1, c.re, 0.00001);
+        assertEquals("a * a.reciprocal im", 0, c.im, 0.00001);
+        for (double angle : new double[] { 0, 0.1, 1, Math.E, Math.PI, 5, 10, -1, -5 })
+        {
+            c = a.rotate(angle);
+            assertEquals("rotated a norm", a.norm(), c.norm(), 0.00001);
+            assertEquals("rotation difference", AngleUtil.normalizeAroundZero(a.phi() + angle), c.phi(), 0.000001);
+        }
+        c = a.divideBy(b);
+        assertEquals("norm of division", a.norm() / b.norm(), c.norm(), 0.0000001);
+        assertEquals("phi of division", AngleUtil.normalizeAroundZero(a.phi() - b.phi()), c.phi(), 0.000001);
+        c = a.divideBy(123);
+        assertEquals("dividend re", a.re / 123, c.re, 0.0000001);
+        assertEquals("dividend im", a.im / 123, c.im, 0.0000001);
+        c = Complex.ZERO.divideBy(Complex.ZERO);
+        assertTrue("ZERO / ZERO re is NaN", Double.isNaN(c.re));
+        assertTrue("ZERO / ZERO im is NaN", Double.isNaN(c.im));
+        c = Complex.ONE.divideBy(Complex.ZERO);
+        assertTrue("ONE / ZERO re is positive Infinity", Double.isInfinite(c.re) && c.re > 0);
+        assertTrue("ONE / ZERO im is NaN", Double.isNaN(c.im));
+        c = Complex.ZERO.minus(Complex.ONE).divideBy(Complex.ZERO);
+        assertTrue("minus ONE / ZERO re is negative Infinity", Double.isInfinite(c.re) && c.re < 0);
+        assertTrue("minus ONE / ZERO im is NaN", Double.isNaN(c.im));
+        c = Complex.I.divideBy(Complex.ZERO);
+        assertTrue("I / ZERO re is NaN", Double.isNaN(c.re));
+        assertTrue("I / ZERO im is positive Infinity", Double.isInfinite(c.im) && c.im > 0);
+        c = Complex.ZERO.minus(Complex.I).divideBy(Complex.ZERO);
+        assertTrue("minus I / ZERO re is NaN", Double.isNaN(c.re));
+        assertTrue("minus I / ZERO im is positive Infinity", Double.isInfinite(c.im) && c.im < 0);
+        c = Complex.ZERO.reciprocal();
+        assertTrue("reciprocal of ZERO re is positive Infinity ", Double.isInfinite(c.re) && c.re > 0);
+        assertTrue("reciprocal of ZERO im is positive Infinity ", Double.isInfinite(c.im) && c.im > 0);
+        c = Complex.ONE.divideBy(Complex.I);
+        assertEquals("ONE / I re is 0", 0, c.re, 0);
+        assertEquals("ONE / I im is -1", -1, c.im, 0);
+        c = Complex.ZERO.divideBy(0.0);
+        assertTrue("ZERO / 0.0 re is NaN", Double.isNaN(c.re));
+        assertTrue("ZERO / 0.0 im is NaN", Double.isNaN(c.im));
+        c = Complex.ONE.divideBy(0.0);
+        assertTrue("ONE / 0.0 re is positive Infinity", Double.isInfinite(c.re) && c.re > 0);
+        assertTrue("ONE / 0.0 im is NaN", Double.isNaN(c.im));
+        c = Complex.ZERO.minus(Complex.ONE).divideBy(0.0);
+        assertTrue("minus ONE / 0.0 re is negative Infinity", Double.isInfinite(c.re) && c.re < 0);
+        assertTrue("minus ONE / 0.0 im is NaN", Double.isNaN(c.im));
+        c = Complex.I.divideBy(0.0);
+        assertTrue("I / 0.0 re is NaN", Double.isNaN(c.re));
+        assertTrue("I / 0.0 im is positive Infinity", Double.isInfinite(c.im) && c.im > 0);
+        c = Complex.ZERO.minus(Complex.I).divideBy(0.0);
+        assertTrue("minus I / 0.0 re is NaN", Double.isNaN(c.re));
+        assertTrue("minus I / 0.0 im is positive Infinity", Double.isInfinite(c.im) && c.im < 0);
+    }
+
+    /**
+     * Test other methods.
+     */
+    @SuppressWarnings({ "unlikely-arg-type" })
+    @Test
+    public void testOthers()
+    {
+        Complex a = new Complex(12, 34);
+        assertTrue("toString returns something descriptive", a.toString().startsWith("Complex "));
+        assertNotEquals("hashCode takes re into account", a.hashCode(), a.plus(Complex.ONE).hashCode());
+        assertNotEquals("hashCode takes im into account", a.hashCode(), a.plus(Complex.I).hashCode());
+        assertTrue(a.equals(a));
+        assertFalse(a.equals(null));
+        assertFalse(a.equals("This is a String; not a complex"));
+        assertFalse(a.equals(new Complex(12, 35)));
+        assertFalse(a.equals(new Complex(13, 34)));
+        assertEquals(a, new Complex(12, 34));
+    }
+
+    /**
+     * Test the hypot method.
+     */
+    @Test
+    public void testHypot()
+    {
+        assertTrue("hypot(Inf, 0)", Double.isInfinite(Complex.hypot(Double.POSITIVE_INFINITY, 0)));
+        assertTrue("hypot(0, Inf)", Double.isInfinite(Complex.hypot(0, Double.POSITIVE_INFINITY)));
+        assertTrue("hypot(-Inf, 0)", Double.isInfinite(Complex.hypot(Double.NEGATIVE_INFINITY, 0)));
+        assertTrue("hypot(0, Inf)", Double.isInfinite(Complex.hypot(0, Double.NEGATIVE_INFINITY)));
+        assertEquals("hypot(0, 0)", 0, Complex.hypot(0, 0), 0);
+        assertTrue("hypot(Nan, 0)", Double.isNaN(Complex.hypot(Double.NaN, 0)));
+        assertTrue("hypot(0, Nan)", Double.isNaN(Complex.hypot(0, Double.NaN)));
+        assertTrue("hypot(Nan, Nan)", Double.isNaN(Complex.hypot(Double.NaN, Double.NaN)));
+        assertEquals("hypot(1, 1", Math.sqrt(2), Complex.hypot(1, 1), 0.000001);
+        assertEquals("hypot(Double.MAX_VALUE, 0)", Double.MAX_VALUE, Complex.hypot(Double.MAX_VALUE, 0),
+                Double.MAX_VALUE / 100000);
+        assertEquals("hypot(0, Double.MAX_VALUE)", Double.MAX_VALUE, Complex.hypot(0, Double.MAX_VALUE),
+                Double.MAX_VALUE / 100000);
+        assertEquals("hypot(Double.MIN_VALUE, 0)", Double.MIN_VALUE, Complex.hypot(Double.MIN_VALUE, 0), 0);
+        assertEquals("hypot(0, Double.MIN_VALUE)", Double.MIN_VALUE, Complex.hypot(0, Double.MIN_VALUE), 0);
+        assertEquals("hypot(Double.MAX_VALUE / 2, Double.MAX_VALUE / 2)", Double.MAX_VALUE / Math.sqrt(2),
+                Complex.hypot(Double.MAX_VALUE / 2, Double.MAX_VALUE / 2), Double.MAX_VALUE / 100000);
+        assertEquals("hypot(Double.MIN_VALUE * 10, Double.MIN_VALUE * 10)", Double.MIN_VALUE * 10 *  Math.sqrt(2),
+                Complex.hypot(Double.MIN_VALUE * 10, Double.MIN_VALUE * 10), Double.MIN_VALUE);
+        assertEquals("hypot is symmetrical in x", Complex.hypot(10, 0), Complex.hypot(-10, 0), 0);
+        assertEquals("hypot is symmetrical in y", Complex.hypot(0, 10), Complex.hypot(0, -10), 0);
+    }
+
+}
---
+++ release/1.06.29/djutils/src/test/java/org/djutils/immutablecollections/TestImmutableTreeSet.java
@@ -0,0 +1,450 @@
+package org.djutils.immutablecollections;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NavigableSet;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * TestImmutableTreeSet.java.
+ * <p>
+ * Copyright (c) 2002-2022 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank"> Alexander Verbraeck</a>
+ */
+public class TestImmutableTreeSet
+{
+
+    /**
+     * Test the tree set.
+     */
+    @Test
+    public final void testTreeSet()
+    {
+        Set<Integer> intSet = new TreeSet<>(Arrays.asList(new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+        NavigableSet<Integer> sortedSet = new TreeSet<Integer>(intSet);
+        testIntSet(sortedSet, new ImmutableTreeSet<Integer>(sortedSet, Immutable.WRAP), Immutable.WRAP);
+        sortedSet = new TreeSet<Integer>(intSet);
+        testIntSet(sortedSet, new ImmutableTreeSet<Integer>(sortedSet, Immutable.COPY), Immutable.COPY);
+        sortedSet = new TreeSet<Integer>(intSet);
+        testIntSet(sortedSet, new ImmutableTreeSet<Integer>(sortedSet), Immutable.COPY);
+        sortedSet = new TreeSet<Integer>(intSet);
+        ImmutableTreeSet<Integer> ihs = new ImmutableTreeSet<Integer>(sortedSet);
+        testIntSet(sortedSet, new ImmutableTreeSet<Integer>(ihs), Immutable.COPY);
+
+        sortedSet = new TreeSet<Integer>(intSet);
+        List<Integer> il = Arrays.asList(new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        testIntSet(sortedSet, new ImmutableTreeSet<Integer>(il), Immutable.COPY);
+        ImmutableTreeSet<Integer> its = new ImmutableTreeSet<Integer>(sortedSet);
+        Assert.assertTrue("toString returns something descriptive", its.toString().startsWith("ImmutableTreeSet ["));
+
+        ImmutableTreeSet<Integer> wrapped = new ImmutableTreeSet<Integer>(its, Immutable.WRAP);
+        Assert.assertEquals("wrapped is equal wrapped-wrapped", its, wrapped);
+        ImmutableTreeSet<Integer> copied = new ImmutableTreeSet<Integer>(its, Immutable.COPY);
+        Assert.assertEquals("wrapped is equal to copy-wrapped", its, copied);
+        Assert.assertEquals("copy-wrapped is equal to wrapped", copied, its);
+    }
+
+    /**
+     * ...
+     * @param set NavigableSet&lt;Integer&gt;; set
+     * @param imSet ImmutableTreeSet&lt;Integer&gt;; immutable set
+     * @param copyOrWrap Immutable;
+     */
+    private void testIntSet(final NavigableSet<Integer> set, final ImmutableTreeSet<Integer> imSet, final Immutable copyOrWrap)
+    {
+        Assert.assertTrue(set.size() == 10);
+        Assert.assertTrue(imSet.size() == 10);
+        for (int i = 0; i < 10; i++)
+        {
+            Assert.assertTrue(imSet.contains(i + 1));
+        }
+        Assert.assertFalse(imSet.isEmpty());
+        Assert.assertFalse(imSet.contains(15));
+
+        Assert.assertTrue(imSet.first() == 1);
+        Assert.assertTrue(imSet.last() == 10);
+
+        if (copyOrWrap == Immutable.COPY)
+        {
+            Assert.assertTrue(imSet.isCopy());
+            Assert.assertTrue(imSet.toSet().equals(set));
+            Assert.assertFalse(imSet.toSet() == set);
+        }
+        else
+        {
+            Assert.assertTrue(imSet.isWrap());
+            Assert.assertTrue(imSet.toSet().equals(set));
+            Assert.assertFalse(imSet.toSet() == set); // this WRAP method returns a NEW list
+        }
+
+        Set<Integer> to = imSet.toSet();
+        Assert.assertTrue(set.equals(to));
+
+        Integer[] arr = imSet.toArray(new Integer[] {});
+        Integer[] sar = set.toArray(new Integer[] {});
+        Assert.assertArrayEquals(arr, sar);
+
+        // modify the underlying data structure
+        set.add(11);
+        if (copyOrWrap == Immutable.COPY)
+        {
+            Assert.assertTrue(imSet.size() == 10);
+        }
+        else
+        {
+            Assert.assertTrue(imSet.size() == 11);
+        }
+    }
+
+    /**
+     * Test the comparator of the ImmutableTreeSet.
+     */
+    @Test
+    public void testComparator()
+    {
+        Integer[] values = new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Set<Integer> intSet = new TreeSet<>(Arrays.asList(values));
+        NavigableSet<Integer> sortedSet = new TreeSet<Integer>(intSet);
+        assertNull("Sorted set uses default compare; not an explicit comparator", sortedSet.comparator());
+        Comparator<Integer> reverseIntegerComparator = new Comparator<Integer>()
+        {
+            @Override
+            public int compare(final Integer o1, final Integer o2)
+            {
+                return -Integer.compare(o1, o2);
+            }
+
+            @Override
+            public String toString()
+            {
+                return "Reversing comparator";
+            }
+        };
+        sortedSet = new TreeSet<Integer>(reverseIntegerComparator);
+        sortedSet.addAll(intSet);
+        ImmutableTreeSet<Integer> its = new ImmutableTreeSet<>(sortedSet, Immutable.WRAP);
+        assertEquals("custom comparator is returned", reverseIntegerComparator, its.comparator());
+        // Let's check that the custom comparator actually worked
+        assertEquals("size must match", values.length, its.size());
+        Integer prev = null;
+        for (Integer value : its)
+        {
+            // System.out.println(value);
+            if (prev != null)
+            {
+                assertTrue("Values must be in non-increasing order", value <= prev);
+            }
+            prev = value;
+        }
+        ImmutableSortedSet<Integer> subSet = its.subSet(7, 3);
+        prev = null;
+        boolean seen3 = false;
+        boolean seen7 = false;
+        for (Integer value : subSet)
+        {
+            // System.out.println(value);
+            assertTrue("value must be in range", value <= 7 && value >= 3);
+            if (3 == value)
+            {
+                seen3 = true;
+            }
+            if (7 == value)
+            {
+                seen7 = true;
+            }
+            if (prev != null)
+            {
+                assertTrue("Values are in decreasing order", value <= prev);
+            }
+            prev = value;
+        }
+        assertFalse("3 must not have been returned", seen3);
+        assertTrue("7 must have been returned", seen7);
+
+        subSet = its.subSet(7, false, 3, false);
+        prev = null;
+        seen3 = false;
+        seen7 = false;
+        for (Integer value : subSet)
+        {
+            // System.out.println(value);
+            assertTrue("value must be in range", value <= 7 && value >= 3);
+            if (3 == value)
+            {
+                seen3 = true;
+            }
+            if (7 == value)
+            {
+                seen7 = true;
+            }
+            if (prev != null)
+            {
+                assertTrue("Values are in decreasing order", value <= prev);
+            }
+            prev = value;
+        }
+        assertFalse("3 must not have been returned", seen3);
+        assertFalse("7 must not have been returned", seen7);
+
+        subSet = its.subSet(7, true, 3, false);
+        prev = null;
+        seen3 = false;
+        seen7 = false;
+        for (Integer value : subSet)
+        {
+            // System.out.println(value);
+            assertTrue("value must be in range", value <= 7 && value >= 3);
+            if (3 == value)
+            {
+                seen3 = true;
+            }
+            if (7 == value)
+            {
+                seen7 = true;
+            }
+            if (prev != null)
+            {
+                assertTrue("Values are in decreasing order", value <= prev);
+            }
+            prev = value;
+        }
+        assertFalse("3 must not have been returned", seen3);
+        assertTrue("7 must have been returned", seen7);
+
+        subSet = its.subSet(7, false, 3, true);
+        prev = null;
+        seen3 = false;
+        seen7 = false;
+        for (Integer value : subSet)
+        {
+            // System.out.println(value);
+            assertTrue("value must be in range", value <= 7 && value >= 3);
+            if (3 == value)
+            {
+                seen3 = true;
+            }
+            if (7 == value)
+            {
+                seen7 = true;
+            }
+            if (prev != null)
+            {
+                assertTrue("Values are in decreasing order", value <= prev);
+            }
+            prev = value;
+        }
+        assertTrue("3 must have been returned", seen3);
+        assertFalse("7 must not have been returned", seen7);
+
+        subSet = its.subSet(7, true, 3, true);
+        prev = null;
+        seen3 = false;
+        seen7 = false;
+        for (Integer value : subSet)
+        {
+            // System.out.println(value);
+            assertTrue("value must be in range", value <= 7 && value >= 3);
+            if (3 == value)
+            {
+                seen3 = true;
+            }
+            if (7 == value)
+            {
+                seen7 = true;
+            }
+            if (prev != null)
+            {
+                assertTrue("Values are in decreasing order", value <= prev);
+            }
+            prev = value;
+        }
+        assertTrue("3 must have been returned", seen3);
+        assertTrue("7 must have been returned", seen7);
+
+        ImmutableSortedSet<Integer> headSet = its.headSet(7);
+        prev = null;
+        seen7 = false;
+        for (Integer value : headSet)
+        {
+            assertTrue("value must be in range", value >= 7);
+            if (7 == value)
+            {
+                seen7 = true;
+            }
+            if (prev != null)
+            {
+                assertTrue("Values are in decreasing order", value <= prev);
+            }
+            prev = value;
+        }
+        assertFalse("7 must not have been returned", seen7);
+
+        headSet = its.headSet(7, true);
+        prev = null;
+        seen7 = false;
+        for (Integer value : headSet)
+        {
+            assertTrue("value must be in range", value >= 7);
+            if (7 == value)
+            {
+                seen7 = true;
+            }
+            if (prev != null)
+            {
+                assertTrue("Values are in decreasing order", value <= prev);
+            }
+            prev = value;
+        }
+        assertTrue("7 must have been returned", seen7);
+
+        headSet = its.headSet(7, false);
+        prev = null;
+        seen7 = false;
+        for (Integer value : headSet)
+        {
+            assertTrue("value must be in range", value >= 7);
+            if (7 == value)
+            {
+                seen7 = true;
+            }
+            if (prev != null)
+            {
+                assertTrue("Values are in decreasing order", value <= prev);
+            }
+            prev = value;
+        }
+        assertFalse("7 must not have been returned", seen7);
+
+        ImmutableSortedSet<Integer> tailSet = its.tailSet(3);
+        prev = null;
+        seen3 = false;
+        for (Integer value : tailSet)
+        {
+            assertTrue("value must be in range", value <= 3);
+            if (3 == value)
+            {
+                seen3 = true;
+            }
+            if (prev != null)
+            {
+                assertTrue("Values are in decreasing order", value <= prev);
+            }
+            prev = value;
+        }
+        assertTrue("3 must have been returned", seen3);
+
+        tailSet = its.tailSet(3, true);
+        prev = null;
+        seen3 = false;
+        for (Integer value : tailSet)
+        {
+            assertTrue("value must be in range", value <= 3);
+            if (3 == value)
+            {
+                seen3 = true;
+            }
+            if (prev != null)
+            {
+                assertTrue("Values are in decreasing order", value <= prev);
+            }
+            prev = value;
+        }
+        assertTrue("3 must have been returned", seen3);
+
+        tailSet = its.tailSet(3, false);
+        prev = null;
+        seen3 = false;
+        for (Integer value : tailSet)
+        {
+            assertTrue("value must be in range", value <= 3);
+            if (3 == value)
+            {
+                seen3 = true;
+            }
+            if (prev != null)
+            {
+                assertTrue("Values are in decreasing order", value <= prev);
+            }
+            prev = value;
+        }
+        assertFalse("3 must not have been returned", seen3);
+
+        for (int index = 0; index < values.length; index++)
+        {
+            Integer lower = its.lower(values[index]);
+            if (index == values.length - 1)
+            {
+                assertNull("lower of last element should have returned null", lower);
+            }
+            else
+            {
+                assertEquals("lower should have returned next higher value", values[index + 1], lower);
+            }
+            Integer higher = its.higher(values[index]);
+            if (index == 0)
+            {
+                assertNull("higher should have returned null", higher);
+            }
+            else
+            {
+                assertEquals("higher should have returned next lower value", values[index - 1], higher);
+            }
+            Integer floor = its.floor(values[index]);
+            assertEquals("floor of element in set returns that element", values[index], floor);
+            Integer ceil = its.floor(values[index]);
+            assertEquals("ceil of element in set returns that element", values[index], ceil);
+        }
+        assertNull("floor of value higher than any in set returns null", its.floor(11));
+        assertNull("ceil of value lower than any in set returns null", its.ceiling(0));
+        assertEquals("floor of value lower than any in set is lowest in set", 1, its.floor(0), 0);
+        assertEquals("ceiling of value higher than any in set is highest in set", 10, its.ceiling(11), 0);
+        ImmutableSet<Integer> descendingSet = its.descendingSet();
+        assertEquals("descendingSet has correct size", values.length, descendingSet.size());
+        prev = null;
+        for (Integer value : descendingSet)
+        {
+            if (null != prev)
+            {
+                assertTrue("descendingSet has value in ascending order", value >= prev);
+            }
+            prev = value;
+        }
+        ImmutableIterator<Integer> ii = its.descendingIterator();
+        prev = null;
+        while (ii.hasNext())
+        {
+            Integer next = ii.next();
+            if (null != prev)
+            {
+                assertTrue("descendingSet has value in ascending order", next >= prev);
+            }
+            prev = next;
+        }
+        try
+        {
+            ii.next();
+            fail("next should have thrown an Exception");
+        }
+        catch (NoSuchElementException nsee)
+        {
+            // Ignore expected exception
+        }
+    }
+
+}
