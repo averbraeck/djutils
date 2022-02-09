e93724b9c7878aa6d5374946d5ae6c3e11842a73
averbraeck <averbraeck@6bd9694c-b1be-467f-8798-70e1d16d5ec4>
2022-02-09 04:00:25 +0000

    Deployed djutils version 1.06.31

---
+++ release/1.06.31/djutils-serialization/src/main/java/org/djutils/serialization/serializers/BasicPrimitiveArrayOrMatrixSerializer.java
@@ -0,0 +1,69 @@
+package org.djutils.serialization.serializers;
+
+import org.djutils.serialization.EndianUtil;
+import org.djutils.serialization.SerializationException;
+
+/**
+ * Serializer for primitive data array classes. *
+ * <p>
+ * Copyright (c) 2019-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://djutils.org/docs/current/djutils/licenses.html">DJUTILS License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, <br>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="https://www.tudelft.nl/staff/p.knoppers/">Peter Knoppers</a>
+ * @param <T> array type, e.g. int[]
+ */
+public abstract class BasicPrimitiveArrayOrMatrixSerializer<T extends Object> extends BasicSerializer<T>
+{
+    /** Size of one element of the encoded data. */
+    private final int elementSize;
+
+    /** Number of dimensions of the data. */
+    private final int numberOfDimensions;
+
+    /**
+     * Construct a new BasicPrimitiveArrayOrMatrixSerializer.
+     * @param type byte; the field type (returned by the <code>fieldType</code> method)
+     * @param elementSize int; the number of bytes needed to encode one additional array element
+     * @param dataClassName String; returned by the dataClassName method
+     * @param numberOfDimensions int; number of dimensions (1 for array, 2 for matrix)
+     */
+    public BasicPrimitiveArrayOrMatrixSerializer(final byte type, final int elementSize, final String dataClassName,
+            final int numberOfDimensions)
+    {
+        super(type, dataClassName);
+        this.elementSize = elementSize;
+        this.numberOfDimensions = numberOfDimensions;
+    }
+
+    @Override
+    public final int sizeWithPrefix(final T object) throws SerializationException
+    {
+        return 1 + size(object);
+    }
+
+    @Override
+    public final void serializeWithPrefix(final T object, final byte[] buffer, final Pointer pointer,
+            final EndianUtil endianUtil) throws SerializationException
+    {
+        buffer[pointer.getAndIncrement(1)] = fieldType();
+        serialize(object, buffer, pointer, endianUtil);
+    }
+
+    /**
+     * Retrieve the number of bytes needed to encode one additional array element.
+     * @return int; the number of bytes needed to encode one additional array element
+     */
+    public final int getElementSize()
+    {
+        return this.elementSize;
+    }
+
+    @Override
+    public final int getNumberOfDimensions()
+    {
+        return this.numberOfDimensions;
+    }
+
+}
---
+++ release/1.06.31/djutils-serialization/src/main/java/org/djutils/serialization/serializers/ObjectSerializer.java
@@ -0,0 +1,49 @@
+package org.djutils.serialization.serializers;
+
+import org.djutils.serialization.EndianUtil;
+import org.djutils.serialization.SerializationException;
+
+/**
+ * Serializer for simple classes.
+ * <p>
+ * Copyright (c) 2019-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://djutils.org/docs/current/djutils/licenses.html">DJUTILS License</a>.
+ * <p>
+ * @version $Revision$, $LastChangedDate$, by $Author$, <br>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @author <a href="https://www.tudelft.nl/staff/p.knoppers/">Peter Knoppers</a>
+ * @param <T> class
+ */
+public abstract class ObjectSerializer<T extends Object> extends BasicSerializer<T>
+{
+    /**
+     * Construct a new ObjectSerializer.
+     * @param type byte; the field type (returned by the <code>fieldType</code> method)
+     * @param dataClassName String; returned by the dataClassName method
+     */
+    public ObjectSerializer(final byte type, final String dataClassName)
+    {
+        super(type, dataClassName);
+    }
+
+    @Override
+    public final int sizeWithPrefix(final T object) throws SerializationException
+    {
+        return 1 + size(object);
+    }
+
+    @Override
+    public final void serializeWithPrefix(final T object, final byte[] buffer, final Pointer pointer,
+            final EndianUtil endianUtil) throws SerializationException
+    {
+        buffer[pointer.getAndIncrement(1)] = fieldType();
+        serialize(object, buffer, pointer, endianUtil);
+    }
+
+    @Override
+    public final int getNumberOfDimensions()
+    {
+        return 0;
+    }
+
+}
