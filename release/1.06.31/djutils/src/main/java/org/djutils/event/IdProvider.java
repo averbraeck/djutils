6041ed8613b987778e8929f6a5bc4a45d66790da
averbraeck <averbraeck@6bd9694c-b1be-467f-8798-70e1d16d5ec4>
2022-02-09 04:00:11 +0000

    Deployed djutils version 1.06.31

---
+++ release/1.06.31/djutils/src/main/java/org/djutils/event/IdProvider.java
@@ -0,0 +1,22 @@
+package org.djutils.event;
+
+import java.io.Serializable;
+
+/**
+ * This functional interface provides a sourceId to an EventProducer.
+ * <p>
+ * Copyright (c) 2020-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://djutils.org/docs/license.html">DJUTILS License</a>.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank">Alexander Verbraeck</a>
+ */
+public interface IdProvider extends Serializable
+{
+    /**
+     * Return an id that identifies the EventProducer, e.g., its toString(), or a unique name by which the EventListener can
+     * identify the sender of an event.
+     * @return Serializable; an id that identifies the EventProducer
+     */
+    Serializable id();
+
+}
