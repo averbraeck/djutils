7154c5ab1247aa2e7aa15140941c4558ec6e963d
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-01-04 01:00:27 +0100

    The LocalEventProducer now extends the 'rich' EventProducer interface.
    All method implementations are embedded in the interface (addListener,
    removeListener, fireEvet, fireTimedEvent, etc.). The LocalEventProducer
    wraps these methods because it is not expected to throw a
    RemoteException.

--- djutils/src/main/java/org/djutils/event/LocalEventProducer.java
+++ djutils/src/main/java/org/djutils/event/LocalEventProducer.java
@@ -2,16 +2,11 @@ package org.djutils.event;
 
 import java.io.Serializable;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 import org.djutils.event.reference.Reference;
 import org.djutils.event.reference.ReferenceType;
-import org.djutils.event.reference.StrongReference;
-import org.djutils.event.reference.WeakReference;
-import org.djutils.exceptions.Throw;
 
 /**
  * The LocalEventProducer defines the registration and fireEvent operations of an event producer. This behavior includes adding
@@ -48,224 +43,9 @@ public class LocalEventProducer implements EventProducer, Serializable
 
     /** {@inheritDoc} */
     @Override
-    public final synchronized boolean addListener(final EventListener listener, final EventType eventType, final int position,
-            final ReferenceType referenceType)
+    public EventListenerMap getEventListenerMap()
     {
-        Throw.whenNull(listener, "listener cannot be null");
-        Throw.whenNull(eventType, "eventType cannot be null");
-        Throw.whenNull(referenceType, "referenceType cannot be null");
-        if (position < LAST_POSITION)
-        {
-            return false;
-        }
-        Reference<EventListener> reference = null;
-        if (referenceType.isStrong())
-        {
-            reference = new StrongReference<EventListener>(listener);
-        }
-        else
-        {
-            reference = new WeakReference<EventListener>(listener);
-        }
-        if (this.eventListenerMap.containsKey(eventType))
-        {
-            for (Reference<EventListener> entry : this.eventListenerMap.get(eventType))
-            {
-                if (listener.equals(entry.get()))
-                {
-                    return false;
-                }
-            }
-            List<Reference<EventListener>> entries = this.eventListenerMap.get(eventType);
-            if (position == LAST_POSITION)
-            {
-                entries.add(reference);
-            }
-            else
-            {
-                entries.add(position, reference);
-            }
-        }
-        else
-        {
-            List<Reference<EventListener>> entries = new ArrayList<>();
-            entries.add(reference);
-            this.eventListenerMap.put(eventType, entries);
-        }
-        return true;
-    }
-
-    /**
-     * Transmit an event to a listener. This method is a hook method. The default implementation simply invokes the notify on
-     * the listener. In specific cases (filtering, storing, queueing, this method can be overwritten.
-     * @param listener EventListenerInterface; the listener for this event
-     * @param event Event; the event to fire
-     * @throws RemoteException on network failure
-     */
-    protected void fireEvent(final EventListener listener, final Event event) throws RemoteException
-    {
-        listener.notify(event);
-    }
-
-    /**
-     * Transmit an event to all subscribed listeners.
-     * @param event Event; the event
-     */
-    public synchronized void fireEvent(final Event event)
-    {
-        Throw.whenNull(event, "event may not be null");
-        if (this.eventListenerMap.containsKey(event.getType()))
-        {
-            // make a safe copy because of possible removeListener() in notify() method during fireEvent
-            List<Reference<EventListener>> listenerList = new ArrayList<>(this.eventListenerMap.get(event.getType()));
-            for (Reference<EventListener> reference : listenerList)
-            {
-                EventListener listener = reference.get();
-                try
-                {
-                    if (listener != null)
-                    {
-                        // The garbage collection has not cleaned the referent
-                        this.fireEvent(listener, event);
-                    }
-                    else
-                    {
-                        // The garbage collection cleaned the referent;
-                        // there is no need to keep the subscription
-                        this.removeListener(reference, event.getType());
-                    }
-                }
-                catch (RemoteException remoteException)
-                {
-                    // A network failure prevented the delivery,
-                    // subscription is removed.
-                    this.removeListener(reference, event.getType());
-                }
-            }
-        }
-    }
-
-    /**
-     * Remove one reference from the subscription list.
-     * @param reference Reference&lt;EventListenerInterface&gt;; the (strong or weak) reference to remove
-     * @param eventType EventType; the eventType for which reference must be removed
-     * @return boolean; true if the reference was removed; otherwise false
-     */
-    private synchronized boolean removeListener(final Reference<EventListener> reference, final EventType eventType)
-    {
-        Throw.whenNull(reference, "reference may not be null");
-        Throw.whenNull(eventType, "eventType may not be null");
-        boolean success = false;
-        for (Iterator<Reference<EventListener>> i = this.eventListenerMap.get(eventType).iterator(); i.hasNext();)
-        {
-            if (i.next().equals(reference))
-            {
-                i.remove();
-                success = true;
-            }
-        }
-        if (this.eventListenerMap.get(eventType).size() == 0)
-        {
-            this.eventListenerMap.remove(eventType);
-        }
-        return success;
-    }
-
-    /**
-     * Transmit a time-stamped event to all interested listeners.
-     * @param event TimedEvent&lt;C&gt;; the event
-     * @param <C> the comparable type to indicate the time when the event is fired
-     */
-    public <C extends Comparable<C> & Serializable> void fireTimedEvent(final TimedEvent<C> event)
-    {
-        fireEvent(event);
-    }
-
-    /**
-     * Transmit an event with no payload object to all interested listeners.
-     * @param eventType EventType; the eventType of the event
-     */
-    public void fireEvent(final EventType eventType)
-    {
-        fireEvent(new Event(eventType, null, true));
-    }
-
-    /**
-     * Transmit a time-stamped event with a no payload object to all interested listeners.
-     * @param eventType EventType; the eventType of the event.
-     * @param time C; a time stamp for the event
-     * @param <C> the comparable type to indicate the time when the event is fired
-     */
-    public <C extends Comparable<C> & Serializable> void fireTimedEvent(final EventType eventType, final C time)
-
-    {
-        fireEvent(new TimedEvent<C>(eventType, null, time, true));
-    }
-
-    /**
-     * Transmit an event with a serializable object as payload to all interested listeners.
-     * @param eventType EventType; the eventType of the event
-     * @param value Serializable; the object sent with the event
-     */
-    public void fireEvent(final EventType eventType, final Serializable value)
-    {
-        fireEvent(new Event(eventType, value, true));
-    }
-
-    /**
-     * Transmit a time-stamped event with a Serializable object (payload) to all interested listeners.
-     * @param eventType EventType; the eventType of the event.
-     * @param value Serializable; the payload sent with the event
-     * @param time C; a time stamp for the event
-     * @param <C> the comparable type to indicate the time when the event is fired
-     */
-    public <C extends Comparable<C> & Serializable> void fireTimedEvent(final EventType eventType, final Serializable value,
-            final C time)
-    {
-        fireEvent(new TimedEvent<C>(eventType, value, time, true));
-    }
-
-    /**
-     * Transmit an event with no payload object to all interested listeners.
-     * @param eventType EventType; the eventType of the event
-     */
-    public void fireUnverifiedEvent(final EventType eventType)
-    {
-        fireEvent(new Event(eventType, null, false));
-    }
-
-    /**
-     * Transmit a time-stamped event with a no payload object to all interested listeners.
-     * @param eventType EventType; the eventType of the event.
-     * @param time C; a time stamp for the event
-     * @param <C> the comparable type to indicate the time when the event is fired
-     */
-    public <C extends Comparable<C> & Serializable> void fireUnverifiedTimedEvent(final EventType eventType, final C time)
-    {
-        fireEvent(new TimedEvent<C>(eventType, null, time, false));
-    }
-
-    /**
-     * Transmit an event with a serializable object as payload to all interested listeners.
-     * @param eventType EventType; the eventType of the event
-     * @param value Serializable; the object sent with the event
-     */
-    public void fireUnverifiedEvent(final EventType eventType, final Serializable value)
-    {
-        fireEvent(new Event(eventType, value, false));
-    }
-
-    /**
-     * Transmit a time-stamped event with a Serializable object (payload) to all interested listeners.
-     * @param eventType EventType; the eventType of the event.
-     * @param value Serializable; the payload sent with the event
-     * @param time C; a time stamp for the event
-     * @param <C> the comparable type to indicate the time when the event is fired
-     */
-    public <C extends Comparable<C> & Serializable> void fireUnverifiedTimedEvent(final EventType eventType,
-            final Serializable value, final C time)
-    {
-        fireEvent(new TimedEvent<C>(eventType, value, time, false));
+        return this.eventListenerMap;
     }
 
     /* **************************************************************************************************** */
@@ -274,9 +54,17 @@ public class LocalEventProducer implements EventProducer, Serializable
 
     /** {@inheritDoc} */
     @Override
-    public EventListenerMap getEventListenerMap()
+    public boolean addListener(final EventListener listener, final EventType eventType, final int position,
+            final ReferenceType referenceType)
     {
-        return this.eventListenerMap;
+        try
+        {
+            return EventProducer.super.addListener(listener, eventType, position, referenceType);
+        }
+        catch (RemoteException exception)
+        {
+            throw new RuntimeException(exception);
+        }
     }
 
     /** {@inheritDoc} */
@@ -420,6 +208,160 @@ public class LocalEventProducer implements EventProducer, Serializable
         try
         {
             return EventProducer.super.getEventTypesWithListeners();
+        }
+        catch (RemoteException exception)
+        {
+            throw new RuntimeException(exception);
+
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void fireEvent(final Event event)
+    {
+        try
+        {
+            EventProducer.super.fireEvent(event);
+        }
+        catch (RemoteException exception)
+        {
+            throw new RuntimeException(exception);
+
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public <C extends Comparable<C> & Serializable> void fireTimedEvent(final TimedEvent<C> event)
+    {
+        try
+        {
+            EventProducer.super.fireTimedEvent(event);
+        }
+        catch (RemoteException exception)
+        {
+            throw new RuntimeException(exception);
+
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void fireEvent(final EventType eventType)
+    {
+        try
+        {
+            EventProducer.super.fireEvent(eventType);
+        }
+        catch (RemoteException exception)
+        {
+            throw new RuntimeException(exception);
+
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public <C extends Comparable<C> & Serializable> void fireTimedEvent(final EventType eventType, final C time)
+    {
+        try
+        {
+            EventProducer.super.fireTimedEvent(eventType, time);
+        }
+        catch (RemoteException exception)
+        {
+            throw new RuntimeException(exception);
+
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void fireEvent(final EventType eventType, final Serializable value)
+    {
+        try
+        {
+            EventProducer.super.fireEvent(eventType, value);
+        }
+        catch (RemoteException exception)
+        {
+            throw new RuntimeException(exception);
+
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public <C extends Comparable<C> & Serializable> void fireTimedEvent(final EventType eventType, final Serializable value,
+            final C time)
+
+    {
+        try
+        {
+            EventProducer.super.fireTimedEvent(eventType, value, time);
+        }
+        catch (RemoteException exception)
+        {
+            throw new RuntimeException(exception);
+
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void fireUnverifiedEvent(final EventType eventType)
+    {
+        try
+        {
+            EventProducer.super.fireUnverifiedEvent(eventType);
+        }
+        catch (RemoteException exception)
+        {
+            throw new RuntimeException(exception);
+
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public <C extends Comparable<C> & Serializable> void fireUnverifiedTimedEvent(final EventType eventType, final C time)
+
+    {
+        try
+        {
+            EventProducer.super.fireUnverifiedTimedEvent(eventType, time);
+        }
+        catch (RemoteException exception)
+        {
+            throw new RuntimeException(exception);
+
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void fireUnverifiedEvent(final EventType eventType, final Serializable value)
+    {
+        try
+        {
+            EventProducer.super.fireUnverifiedEvent(eventType, value);
+        }
+        catch (RemoteException exception)
+        {
+            throw new RuntimeException(exception);
+
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public <C extends Comparable<C> & Serializable> void fireUnverifiedTimedEvent(final EventType eventType,
+            final Serializable value, final C time)
+    {
+        try
+        {
+            EventProducer.super.fireUnverifiedTimedEvent(eventType, value, time);
         }
         catch (RemoteException exception)
         {
