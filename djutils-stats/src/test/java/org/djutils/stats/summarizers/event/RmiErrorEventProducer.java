0078864144433784227ace6c59bcabe97a097c35
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-01-16 03:02:30 +0100

    Classes added to test for Remote error testing
    - RmiErrorEventListener
    - RmiErrorEventProducer

---
+++ djutils-stats/src/test/java/org/djutils/stats/summarizers/event/RmiErrorEventProducer.java
@@ -0,0 +1,49 @@
+package org.djutils.stats.summarizers.event;
+
+import java.io.Serializable;
+import java.rmi.RemoteException;
+
+import org.djutils.event.EventListenerMap;
+import org.djutils.event.EventProducer;
+import org.djutils.event.EventType;
+
+/**
+ * EventProducer that throws an error on removeListener().
+ * <p>
+ * Copyright (c) 2023-2023 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class RmiErrorEventProducer implements EventProducer
+{
+    /** */
+    private static final long serialVersionUID = 1L;
+
+    /** The collection of interested listeners. */
+    private EventListenerMap eventListenerMap = new EventListenerMap();
+
+    /** {@inheritDoc} */
+    @Override
+    public EventListenerMap getEventListenerMap()
+    {
+        return this.eventListenerMap;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void fireEvent(final EventType eventType) throws RemoteException
+    {
+        throw new RemoteException("planned");
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void fireEvent(final EventType eventType, final Serializable value) throws RemoteException
+    {
+        throw new RemoteException("planned");
+    }
+
+}
