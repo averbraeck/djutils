ef5bc95aef701dd5602f9d484955980c510c03d0
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-01-16 03:04:50 +0100

    Unit tests expanded
    - Test coverage now ~ 100%
    - All tests use Try.testFail(..) instead of the long try - assert - fail
    - catch - do nothing structure
    - Remote errors emulated and tested

--- djutils-stats/src/test/java/org/djutils/stats/summarizers/event/EventBasedCounterTest.java
+++ djutils-stats/src/test/java/org/djutils/stats/summarizers/event/EventBasedCounterTest.java
@@ -2,13 +2,13 @@ package org.djutils.stats.summarizers.event;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.rmi.RemoteException;
 
 import org.djutils.event.Event;
 import org.djutils.event.EventListener;
 import org.djutils.event.EventType;
+import org.djutils.exceptions.Try;
 import org.djutils.metadata.MetaData;
 import org.junit.Test;
 
@@ -55,15 +55,7 @@ public class EventBasedCounterTest
         assertEquals(0, cel.getCountEvents());
 
         // test wrong event
-        try
-        {
-            counter.notify(new Event(COUNT_EVENT, "abc"));
-            fail("Wrong payload to EventBasedCounter should have triggreed an exception");
-        }
-        catch (RuntimeException exception)
-        {
-            // ok, should have given error
-        }
+        Try.testFail(() -> counter.notify(new Event(COUNT_EVENT, "abc")), IllegalArgumentException.class);
 
         LoggingEventListener nListener = new LoggingEventListener();
         counter.addListener(nListener, StatisticsEvents.N_EVENT);
@@ -83,6 +75,23 @@ public class EventBasedCounterTest
         assertEquals(100, countListener.getNumberOfEvents());
         assertEquals(100L, nListener.getLastEvent().getContent());
         assertEquals(value, countListener.getLastEvent().getContent());
+    }
+
+    /**
+     * Test the counter for RemoteExceptions.
+     * @throws RemoteException on network error for the event-based statistic
+     */
+    @Test
+    public void testEventBasedCounterRemote() throws RemoteException
+    {
+        String description = "counter description";
+        EventBasedCounter counter = new EventBasedCounter(description, new RmiErrorEventProducer());
+        RmiErrorEventListener listener = new RmiErrorEventListener();
+        counter.addListener(listener, StatisticsEvents.INITIALIZED_EVENT);
+        counter.addListener(listener, StatisticsEvents.OBSERVATION_ADDED_EVENT);
+        // RemoteException is packed in a RuntimeException
+        Try.testFail(() -> counter.initialize(), RuntimeException.class);
+        Try.testFail(() -> counter.register(1L), RuntimeException.class);
     }
 
     /** The listener that counts the OBSERVATION_ADDED_EVENT events and checks correctness. */
@@ -111,4 +120,5 @@ public class EventBasedCounterTest
             return this.countEvents;
         }
     }
+
 }
