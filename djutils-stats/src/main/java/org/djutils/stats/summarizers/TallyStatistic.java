67fd2e066bc1200400fe22165980e9e0f599e312
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-01-16 02:44:54 +0100

    Introduce a TallyStatistic class with the min() and max() methods
    Many packages expect min() and max(), e.g., for BoxAndWhisker charts

---
+++ djutils-stats/src/main/java/org/djutils/stats/summarizers/TallyStatistic.java
@@ -0,0 +1,27 @@
+package org.djutils.stats.summarizers;
+
+/**
+ * TallyStatistic makes the common methods of any type of Tally available (unweighted, weighted, timestamped, or event based).
+ * <p>
+ * Copyright (c) 2023-2023 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public interface TallyStatistic extends Statistic
+{
+    /**
+     * Returns the maximum value of any given observation, or NaN when no observations were registered.
+     * @return double; the maximum value of any given observation
+     */
+    double getMax();
+
+    /**
+     * Returns the minimum value of any given observation, or NaN when no observations were registered.
+     * @return double; the minimum value of any given observation
+     */
+    double getMin();
+
+}
