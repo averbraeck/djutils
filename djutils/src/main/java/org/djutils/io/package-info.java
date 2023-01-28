e84903d5f237234430fdfe28f007abb60030b6f9
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-01-28 11:21:15 +0100

    Issue #8. CompressedFileWriter class moved from ots to djutils-base
    The class has been placed in the 'io' package.

--- djutils/src/main/java/org/djutils/io/package-info.java
+++ djutils/src/main/java/org/djutils/io/package-info.java
@@ -1,5 +1,6 @@
 /**
- * Classes to assist in translating URLs to paths, either based on a resource, file, or inside a jar-file.
+ * Classes to assist in translating URLs to paths, either based on a resource, file, or inside a jar-file; and other file
+ * related classes.
  * <p>
  * Copyright (c) 2002-2023 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
