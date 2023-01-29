fbec17f17796f95bbe85a493241f892a5e8bd31d
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-01-29 16:11:44 +0100

    Issue #8. Unit tests for column units in tsv, json and xml files

--- djutils-data/src/test/java/org/djutils/data/TestJsonTable.java
+++ djutils-data/src/test/java/org/djutils/data/TestJsonTable.java
@@ -10,7 +10,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
+import org.djunits.value.vdouble.scalar.Duration;
+import org.djunits.value.vdouble.scalar.Length;
+import org.djunits.value.vdouble.scalar.Speed;
 import org.djutils.data.json.JsonData;
 import org.djutils.data.serialization.TextSerializationException;
 import org.junit.Test;
@@ -73,4 +77,94 @@ public class TestJsonTable
         assertFalse(it1.hasNext());
         assertFalse(it2.hasNext());
     }
+
+    /**
+     * test reading and writing of a JSON file with "missing values" coded as null values.
+     * @throws IOException on error
+     * @throws TextSerializationException on unknown data type for (de)serialization
+     */
+    @Test
+    public void testReadWriteCsvNulls() throws IOException, TextSerializationException
+    {
+        File tempDataFile = File.createTempFile("testdata", ".json");
+        tempDataFile.deleteOnExit();
+
+        Column<Integer> column1 = new Column<>("time", "time, rounded to second [s]", int.class);
+        Column<Length> column2 = new Column<>("value", "measured value [m]", Length.class, "m");
+        Column<String> column3 = new Column<>("remark", "remark about the measurement", String.class);
+        List<Column<?>> columns = List.of(column1, column2, column3);
+        ListTable table1 = new ListTable("tableId", "tableDescription", columns);
+        table1.addRow(new Object[] {1, Length.valueOf("1 mm"), "normal"});
+        table1.addRow(new Object[] {null, Length.valueOf("10.0 km"), "normal"});
+        table1.addRow(new Object[] {3, null, "abnormal"});
+        table1.addRow(new Object[] {4, Length.instantiateSI(40.0), null});
+        JsonData.writeData(tempDataFile.getAbsolutePath(), table1);
+
+        Table table2 = JsonData.readData(tempDataFile.getAbsolutePath());
+        assertTrue(table2 instanceof ListTable);
+        assertEquals(table1.getId(), table2.getId());
+        assertEquals(table1.getDescription(), table2.getDescription());
+        assertEquals(table1.getNumberOfColumns(), table2.getNumberOfColumns());
+        assertArrayEquals(table1.getColumnIds(), table2.getColumnIds());
+        assertArrayEquals(table1.getColumnDescriptions(), table2.getColumnDescriptions());
+        assertArrayEquals(table1.getColumnDataTypes(), table2.getColumnDataTypes());
+        assertArrayEquals(table1.getColumnDataTypeStrings(), table2.getColumnDataTypeStrings());
+
+        Iterator<Row> it1 = table1.iterator();
+        Iterator<Row> it2 = table2.iterator();
+        while (it1.hasNext() && it2.hasNext())
+        {
+            Row r1 = it1.next();
+            Row r2 = it2.next();
+            assertArrayEquals(r1.getValues(), r2.getValues());
+        }
+        assertFalse(it1.hasNext());
+        assertFalse(it2.hasNext());
+    }
+
+    /**
+     * test reading and writing of a JSON file with columns that treat units in different ways.
+     * @throws IOException on error
+     * @throws TextSerializationException on unknown data type for (de)serialization
+     */
+    @Test
+    public void testReadWriteCsvUnits() throws IOException, TextSerializationException
+    {
+        Locale.setDefault(Locale.US);
+
+        File tempDataFile = File.createTempFile("testdata", ".json");
+        tempDataFile.deleteOnExit();
+
+        Column<Duration> column1 = new Column<>("time", "time, rounded to second [s]", Duration.class, "ms");
+        Column<Length> column2 = new Column<>("value", "measured value [m]", Length.class, "m");
+        Column<Speed> column3 = new Column<>("remark", "remark about the measurement", Speed.class, "mi/h");
+        List<Column<?>> columns = List.of(column1, column2, column3);
+        ListTable table1 = new ListTable("tableId", "tableDescription", columns);
+        table1.addRow(new Object[] {Duration.valueOf("1 s"), Length.valueOf("10.0 m"), Speed.valueOf("15.0 mi/h")});
+        table1.addRow(new Object[] {Duration.valueOf("2 h"), Length.valueOf("20.0 km"), Speed.valueOf("25.0 km/h")});
+        table1.addRow(new Object[] {Duration.valueOf("3 ms"), Length.valueOf("30.0 mm"), Speed.valueOf("35.0 m/s")});
+        JsonData.writeData(tempDataFile.getAbsolutePath(), table1);
+
+        Table table2 = JsonData.readData(tempDataFile.getAbsolutePath());
+        assertTrue(table2 instanceof ListTable);
+        assertEquals(table1.getId(), table2.getId());
+        assertEquals(table1.getDescription(), table2.getDescription());
+        assertEquals(table1.getNumberOfColumns(), table2.getNumberOfColumns());
+        assertArrayEquals(table1.getColumnIds(), table2.getColumnIds());
+        assertArrayEquals(table1.getColumnDescriptions(), table2.getColumnDescriptions());
+        assertArrayEquals(table1.getColumnDataTypes(), table2.getColumnDataTypes());
+        assertArrayEquals(table1.getColumnDataTypeStrings(), table2.getColumnDataTypeStrings());
+
+        Iterator<Row> it1 = table1.iterator();
+        Iterator<Row> it2 = table2.iterator();
+        while (it1.hasNext() && it2.hasNext())
+        {
+            Row r1 = it1.next();
+            Row r2 = it2.next();
+            assertArrayEquals(r1.getValues(), r2.getValues());
+        }
+        assertFalse(it1.hasNext());
+        assertFalse(it2.hasNext());
+    }
+
 }
--- djutils-data/src/test/java/org/djutils/data/TestXmlTable.java
+++ djutils-data/src/test/java/org/djutils/data/TestXmlTable.java
@@ -10,9 +10,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
 import javax.xml.stream.XMLStreamException;
 
+import org.djunits.value.vdouble.scalar.Duration;
+import org.djunits.value.vdouble.scalar.Length;
+import org.djunits.value.vdouble.scalar.Speed;
 import org.djutils.data.serialization.TextSerializationException;
 import org.djutils.data.xml.XmlData;
 import org.junit.Test;
@@ -77,4 +81,96 @@ public class TestXmlTable
         assertFalse(it1.hasNext());
         assertFalse(it2.hasNext());
     }
+
+    /**
+     * test reading and writing of a XML file with "missing values" coded as null values.
+     * @throws IOException on error
+     * @throws TextSerializationException on unknown data type for (de)serialization
+     * @throws XMLStreamException on XML error
+     */
+    @Test
+    public void testReadWriteCsvNulls() throws IOException, TextSerializationException, XMLStreamException
+    {
+        File tempDataFile = File.createTempFile("testdata", ".xml");
+        tempDataFile.deleteOnExit();
+
+        Column<Integer> column1 = new Column<>("time", "time, rounded to second [s]", int.class);
+        Column<Length> column2 = new Column<>("value", "measured value [m]", Length.class, "m");
+        Column<String> column3 = new Column<>("remark", "remark about the measurement", String.class);
+        List<Column<?>> columns = List.of(column1, column2, column3);
+        ListTable table1 = new ListTable("tableId", "tableDescription", columns);
+        table1.addRow(new Object[] {1, Length.valueOf("1 mm"), "normal"});
+        table1.addRow(new Object[] {null, Length.valueOf("10.0 km"), "normal"});
+        table1.addRow(new Object[] {3, null, "abnormal"});
+        table1.addRow(new Object[] {4, Length.instantiateSI(40.0), null});
+        XmlData.writeData(tempDataFile.getAbsolutePath(), table1);
+
+        Table table2 = XmlData.readData(tempDataFile.getAbsolutePath());
+        assertTrue(table2 instanceof ListTable);
+        assertEquals(table1.getId(), table2.getId());
+        assertEquals(table1.getDescription(), table2.getDescription());
+        assertEquals(table1.getNumberOfColumns(), table2.getNumberOfColumns());
+        assertArrayEquals(table1.getColumnIds(), table2.getColumnIds());
+        assertArrayEquals(table1.getColumnDescriptions(), table2.getColumnDescriptions());
+        assertArrayEquals(table1.getColumnDataTypes(), table2.getColumnDataTypes());
+        assertArrayEquals(table1.getColumnDataTypeStrings(), table2.getColumnDataTypeStrings());
+
+        Iterator<Row> it1 = table1.iterator();
+        Iterator<Row> it2 = table2.iterator();
+        while (it1.hasNext() && it2.hasNext())
+        {
+            Row r1 = it1.next();
+            Row r2 = it2.next();
+            assertArrayEquals(r1.getValues(), r2.getValues());
+        }
+        assertFalse(it1.hasNext());
+        assertFalse(it2.hasNext());
+    }
+
+    /**
+     * test reading and writing of a XML file with columns that treat units in different ways.
+     * @throws IOException on error
+     * @throws TextSerializationException on unknown data type for (de)serialization
+     * @throws XMLStreamException on XML error
+     */
+    @Test
+    public void testReadWriteCsvUnits() throws IOException, TextSerializationException, XMLStreamException
+    {
+        Locale.setDefault(Locale.US);
+
+        File tempDataFile = File.createTempFile("testdata", ".xml");
+        tempDataFile.deleteOnExit();
+
+        Column<Duration> column1 = new Column<>("time", "time, rounded to second [s]", Duration.class, "ms");
+        Column<Length> column2 = new Column<>("value", "measured value [m]", Length.class, "m");
+        Column<Speed> column3 = new Column<>("remark", "remark about the measurement", Speed.class, "mi/h");
+        List<Column<?>> columns = List.of(column1, column2, column3);
+        ListTable table1 = new ListTable("tableId", "tableDescription", columns);
+        table1.addRow(new Object[] {Duration.valueOf("1 s"), Length.valueOf("10.0 m"), Speed.valueOf("15.0 mi/h")});
+        table1.addRow(new Object[] {Duration.valueOf("2 h"), Length.valueOf("20.0 km"), Speed.valueOf("25.0 km/h")});
+        table1.addRow(new Object[] {Duration.valueOf("3 ms"), Length.valueOf("30.0 mm"), Speed.valueOf("35.0 m/s")});
+        XmlData.writeData(tempDataFile.getAbsolutePath(), table1);
+
+        Table table2 = XmlData.readData(tempDataFile.getAbsolutePath());
+        assertTrue(table2 instanceof ListTable);
+        assertEquals(table1.getId(), table2.getId());
+        assertEquals(table1.getDescription(), table2.getDescription());
+        assertEquals(table1.getNumberOfColumns(), table2.getNumberOfColumns());
+        assertArrayEquals(table1.getColumnIds(), table2.getColumnIds());
+        assertArrayEquals(table1.getColumnDescriptions(), table2.getColumnDescriptions());
+        assertArrayEquals(table1.getColumnDataTypes(), table2.getColumnDataTypes());
+        assertArrayEquals(table1.getColumnDataTypeStrings(), table2.getColumnDataTypeStrings());
+
+        Iterator<Row> it1 = table1.iterator();
+        Iterator<Row> it2 = table2.iterator();
+        while (it1.hasNext() && it2.hasNext())
+        {
+            Row r1 = it1.next();
+            Row r2 = it2.next();
+            assertArrayEquals(r1.getValues(), r2.getValues());
+        }
+        assertFalse(it1.hasNext());
+        assertFalse(it2.hasNext());
+    }
+
 }
