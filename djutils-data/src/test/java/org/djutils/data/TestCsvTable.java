4f990a939149632e962c1f3c51a674c7dde4233b
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-01-29 11:23:30 +0100

    Issue #8. Unit test reading/writing null values in columns in csv file

--- djutils-data/src/test/java/org/djutils/data/TestCsvTable.java
+++ djutils-data/src/test/java/org/djutils/data/TestCsvTable.java
@@ -7,11 +7,11 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
+import org.djunits.value.vdouble.scalar.Length;
 import org.djutils.data.csv.CsvData;
 import org.djutils.data.serialization.TextSerializationException;
 import org.djutils.io.CompressedFileWriter;
@@ -47,10 +47,7 @@ public class TestCsvTable
         Column<Integer> column1 = new Column<>("time", "time, rounded to second [s]", int.class, "");
         Column<Double> column2 = new Column<>("value", "measured value [m]", double.class, "");
         Column<String> column3 = new Column<>("remark", "remark about the measurement", String.class, "");
-        List<Column<?>> columns = new ArrayList<>();
-        columns.add(column1);
-        columns.add(column2);
-        columns.add(column3);
+        List<Column<?>> columns = List.of(column1, column2, column3);
         ListTable table1 = new ListTable("tableId", "tableDescription", columns);
         table1.addRow(new Object[] {1, 5.0, "normal"});
         table1.addRow(new Object[] {2, 10.0, "normal"});
@@ -170,4 +167,52 @@ public class TestCsvTable
         assertFalse(it2.hasNext());
     }
 
+    /**
+     * test reading and writing of a CSV file with "missing values" coded as null values.
+     * @throws IOException on error
+     * @throws TextSerializationException on unknown data type for (de)serialization
+     */
+    @Test
+    public void testReadWriteCsvNulls() throws IOException, TextSerializationException
+    {
+        File tempDataFile = File.createTempFile("testdata", ".csv");
+        File tempMetaDataFile = File.createTempFile("testmetadata", ".csv");
+        tempDataFile.deleteOnExit();
+        tempMetaDataFile.deleteOnExit();
+
+        Column<Integer> column1 = new Column<>("time", "time, rounded to second [s]", int.class);
+        Column<Length> column2 = new Column<>("value", "measured value [m]", Length.class, "m");
+        Column<String> column3 = new Column<>("remark", "remark about the measurement", String.class);
+        List<Column<?>> columns = List.of(column1, column2, column3);
+        ListTable table1 = new ListTable("tableId", "tableDescription", columns);
+        table1.addRow(new Object[] {1, Length.valueOf("1 mm"), "normal"});
+        table1.addRow(new Object[] {null, Length.valueOf("10.0 km"), "normal"});
+        table1.addRow(new Object[] {3, null, "abnormal"});
+        table1.addRow(new Object[] {4, Length.instantiateSI(40.0), null});
+        CsvData.writeData(tempDataFile.getAbsolutePath(), tempMetaDataFile.getAbsolutePath(), table1);
+
+        Table table2 = CsvData.readData(tempDataFile.getAbsolutePath(), tempMetaDataFile.getAbsolutePath());
+        assertTrue(table2 instanceof ListTable);
+        assertEquals(table1.getId(), table2.getId());
+        assertEquals(table1.getDescription(), table2.getDescription());
+        assertEquals(table1.getNumberOfColumns(), table2.getNumberOfColumns());
+        assertArrayEquals(table1.getColumnIds(), table2.getColumnIds());
+        assertArrayEquals(table1.getColumnDescriptions(), table2.getColumnDescriptions());
+        assertArrayEquals(table1.getColumnDataTypes(), table2.getColumnDataTypes());
+        assertArrayEquals(table1.getColumnDataTypeStrings(), table2.getColumnDataTypeStrings());
+
+        Iterator<Row> it1 = table1.iterator();
+        Iterator<Row> it2 = table2.iterator();
+        while (it1.hasNext() && it2.hasNext())
+        {
+            Row r1 = it1.next();
+            Row r2 = it2.next();
+            assertArrayEquals(r1.getValues(), r2.getValues());
+        }
+        assertFalse(it1.hasNext());
+        assertFalse(it2.hasNext());
+    }
+
+    // TODO: check when the column heading is in km, and you offer m whether the value is transformed or not...
+    // TODO: the contract for this is NOT clear.
 }
