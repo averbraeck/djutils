894c1cce910d8758ef877cf231f29bde91cdca70
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-04-29 23:56:25 +0200

    Adapt package-info class of org.djutils.base to newly added classes

--- djutils/src/main/java/org/djutils/base/package-info.java
+++ djutils/src/main/java/org/djutils/base/package-info.java
@@ -1,5 +1,6 @@
 /**
- * The djutils.base package contains a number of very generic classes / interfaces such as the Identifiable interface.
+ * The djutils.base package contains a number of very generic classes / interfaces such as the Identifiable interface, the
+ * mutable primitive classes, and the NumberParser that allows for lenient parsing.
  * <p>
  * Copyright (c) 2020-2023 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://djutils.org/docs/current/djutils/licenses.html">DJUTILS License</a>.
