2150a6c93195fa19c2b22196c0932dbb647ba965
pknoppers <P.Knoppers@tudelft.nl>
2023-08-30 18:52:16 +0200

    Forgot to add and commig the UnitParser interface.

---
+++ djutils-eval/src/main/java/org/djutils/eval/UnitParser.java
@@ -0,0 +1,26 @@
+package org.djutils.eval;
+
+import org.djunits.value.vdouble.scalar.base.DoubleScalar;
+
+/**
+ * UnitParser.java.
+ * <p>
+ * Copyright (c) 2023-2023 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ */
+public interface UnitParser
+{
+    /**
+     * Parse a user-specific unit.
+     * @param value double; the value (already parsed; expressed in the unit)
+     * @param unit String; the unit specification
+     * @return DoubleScalar&lt;?,?&gt;; the resulting (strongly typed) value, or null if the user-specified parser cannot handle
+     *         the provided unit
+     */
+    DoubleScalar<?, ?> parseUnit(final double value, final String unit);
+
+}
