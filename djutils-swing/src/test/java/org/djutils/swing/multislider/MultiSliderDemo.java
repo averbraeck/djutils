cd522334204b77445486ffc5e3914129570adf37
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-11-17 18:21:08 +0100

    Issue #45. Demo vertical slider as well

--- djutils-swing/src/test/java/org/djutils/swing/multislider/MultiSliderDemo.java
+++ djutils-swing/src/test/java/org/djutils/swing/multislider/MultiSliderDemo.java
@@ -1,5 +1,6 @@
 package org.djutils.swing.multislider;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
@@ -26,21 +27,30 @@ public class MultiSliderDemo extends JFrame
     /** */
     public MultiSliderDemo()
     {
-        setPreferredSize(new Dimension(640, 320));
-        var multiSlider = new MultiSlider(SwingConstants.HORIZONTAL, 0, 100, new int[] {25, 50, 75});
-        multiSlider.setMajorTickSpacing(25);
-        multiSlider.setMinorTickSpacing(5);
-        multiSlider.setPaintTicks(true);
-        multiSlider.setPaintLabels(true);
-        getContentPane().add(multiSlider);
+        setPreferredSize(new Dimension(640, 640));
+        getContentPane().setLayout(new BorderLayout(10, 10));
 
+        var horSlider = new MultiSlider(SwingConstants.HORIZONTAL, 0, 100, new int[] {25, 50, 75});
+        horSlider.setMajorTickSpacing(25);
+        horSlider.setMinorTickSpacing(5);
+        horSlider.setPaintTicks(true);
+        horSlider.setPaintLabels(true);
+        getContentPane().add(horSlider, BorderLayout.NORTH);
+        
+        var vertSlider = new MultiSlider(SwingConstants.VERTICAL, 0, 1000, new int[] {400, 600});
+        vertSlider.setMajorTickSpacing(250);
+        vertSlider.setMinorTickSpacing(50);
+        vertSlider.setPaintTicks(true);
+        vertSlider.setPaintLabels(true);
+        getContentPane().add(vertSlider, BorderLayout.WEST);
+        
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
 
-        multiSlider.addChangeListener(new ChangeListener()
+        horSlider.addChangeListener(new ChangeListener()
         {
             @Override
             public void stateChanged(final ChangeEvent e)
@@ -48,10 +58,24 @@ public class MultiSliderDemo extends JFrame
                 MultiSlider s = (MultiSlider) e.getSource();
                 for (int i = 0; i < s.getNumberOfThumbs(); i++)
                 {
-                    System.out.println("Thumb " + i + ": " + s.getValue(i));
+                    System.out.println("Horizontal Thumb " + i + ": " + s.getValue(i));
                 }
             }
         });
+        
+        vertSlider.addChangeListener(new ChangeListener()
+        {
+            @Override
+            public void stateChanged(final ChangeEvent e)
+            {
+                MultiSlider s = (MultiSlider) e.getSource();
+                for (int i = 0; i < s.getNumberOfThumbs(); i++)
+                {
+                    System.out.println("Vertical Thumb " + i + ": " + s.getValue(i));
+                }
+            }
+        });
+
     }
 
     /**
