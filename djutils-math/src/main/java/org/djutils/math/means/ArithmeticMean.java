d9ac304015f7fbf42480313e65dfe35f6ad3b348
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-02-22 22:49:00 +0100

    Change copyright notice to 2025

--- djutils-math/src/main/java/org/djutils/math/means/ArithmeticMean.java
+++ djutils-math/src/main/java/org/djutils/math/means/ArithmeticMean.java
@@ -3,7 +3,7 @@ package org.djutils.math.means;
 /**
  * Compute arithmetic (weighted) mean of a set of values.
  * <p>
- * Copyright (c) 2013-2016 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * Copyright (c) 2013-2025 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://djutils.org/docs/current/djutils/licenses.html">DJUTILS License</a>.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
