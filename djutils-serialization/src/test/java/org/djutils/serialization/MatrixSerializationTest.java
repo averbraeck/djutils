d0c182120880ff8846fc8685f6001b162990883f
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-02-23 00:17:02 +0100

    Include test that was left out for debugging

--- djutils-serialization/src/test/java/org/djutils/serialization/MatrixSerializationTest.java
+++ djutils-serialization/src/test/java/org/djutils/serialization/MatrixSerializationTest.java
@@ -43,7 +43,7 @@ public class MatrixSerializationTest extends AbstractSerializationTest
         Double[][] doubleValues2 = new Double[][] {{-23.45, -34.56, -45.67}, {-22.2, -33.3, -44.4}};
         Object[] objects = new Object[] {integer, integerValues2, shortValues, shortValues2, longValues, longValues2,
                 byteValues, byteValues2, floatValues, floatValues2, doubleValues, doubleValues2, boolValues, boolValues2};
-        for (EndianUtil endianUtil : new EndianUtil[] {/* EndianUtil.BIG_ENDIAN, */EndianUtil.LITTLE_ENDIAN})
+        for (EndianUtil endianUtil : new EndianUtil[] {EndianUtil.BIG_ENDIAN, EndianUtil.LITTLE_ENDIAN})
         {
             for (boolean encodeUTF8 : new boolean[] {false, true})
             {
