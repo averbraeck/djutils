15512d1828e54ecd1f14ec70dcf8ac3840fa740c
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-02-23 02:10:09 +0100

    Ragged matrix test belongs in this class

--- djutils-serialization/src/test/java/org/djutils/serialization/MatrixSerializationTest.java
+++ djutils-serialization/src/test/java/org/djutils/serialization/MatrixSerializationTest.java
@@ -2,6 +2,7 @@ package org.djutils.serialization;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.djutils.decoderdumper.HexDumper;
 import org.djutils.serialization.util.SerialDataDumper;
@@ -61,6 +62,55 @@ public class MatrixSerializationTest extends AbstractSerializationTest
                         assertTrue(deepEquals0(makePrimitive(objects[i]), makePrimitive(decodedObjects[i])),
                                 "decoded object at index " + i + "(" + objects[i] + ") equals corresponding object in input");
                     }
+                }
+            }
+        }
+    }
+
+    /**
+     * Test that jagged matrices are detected and cause a SerializationException.
+     */
+    @Test
+    public void testJaggedMatrices()
+    {
+        int[][] integer = new int[][] {{1, 2, 3}, {5, 6}};
+        Integer[][] integerValues2 = new Integer[][] {{-1, -2}, {-4, -5, -6}};
+        short[][] shortValues = new short[][] {{10, 20}, {40, 50, 60}};
+        Short[][] shortValues2 = new Short[][] {{-10, -20, -30}, {-40, -50}};
+        long[][] longValues = new long[][] {{1000, 2000, 3000}, {3000, 4000}};
+        Long[][] longValues2 = new Long[][] {{-1000L, -2000L}, {-3000L, -4000L, -5000L}};
+        byte[][] byteValues = new byte[][] {{12, 13}, {15, 16, 17}};
+        Byte[][] byteValues2 = new Byte[][] {{-12, -13, -14}, {-15, -16}};
+        boolean[][] boolValues = new boolean[][] {{false, true, true}, {false, false}};
+        Boolean[][] boolValues2 = new Boolean[][] {{true, true}, {true, true, true}};
+        float[][] floatValues = new float[][] {{12.3f, 23.4f}, {44.4f, 55.5f, 66.6f}};
+        Float[][] floatValues2 = new Float[][] {{-12.3f, -23.4f, -34.5f}, {-11.1f, -22.2f}};
+        double[][] doubleValues = new double[][] {{23.45, 34.56, 45.67}, {55.5, 66.6}};
+        Double[][] doubleValues2 = new Double[][] {{-23.45, -34.56}, {-22.2, -33.3, -44.4}};
+        Object[] objects = new Object[] {integer, integerValues2, shortValues, shortValues2, longValues, longValues2,
+                byteValues, byteValues2, floatValues, floatValues2, doubleValues, doubleValues2, boolValues, boolValues2};
+        for (EndianUtil endianUtil : new EndianUtil[] {EndianUtil.BIG_ENDIAN, EndianUtil.LITTLE_ENDIAN})
+        {
+            for (Object object : objects)
+            {
+                Object[] singleObjectArray = new Object[] {object};
+                try
+                {
+                    TypedMessage.encodeUTF16(endianUtil, singleObjectArray);
+                    fail("Jagged array should have thrown a SerializationException");
+                }
+                catch (SerializationException se)
+                {
+                    // Ignore expected exception
+                }
+                try
+                {
+                    TypedMessage.encodeUTF8(endianUtil, singleObjectArray);
+                    fail("Jagged array should have thrown a SerializationException");
+                }
+                catch (SerializationException se)
+                {
+                    // Ignore expected exception
                 }
             }
         }
