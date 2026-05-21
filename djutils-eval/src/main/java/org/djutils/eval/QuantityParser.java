3022f23bbf02f66b45412ece23108c44365e5196
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-05-21 18:51:05 +0200

    Issue #134. Rename UnitParser to QuantityParser

--- djutils-eval/src/main/java/org/djutils/eval/QuantityParser.java
+++ djutils-eval/src/main/java/org/djutils/eval/QuantityParser.java
@@ -1,9 +1,9 @@
 package org.djutils.eval;
 
-import org.djunits.value.vdouble.scalar.base.DoubleScalar;
+import org.djunits.quantity.def.Quantity;
 
 /**
- * UnitParser.java.
+ * Parse a quantity based on value and unit.
  * <p>
  * Copyright (c) 2023-2025 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
@@ -12,15 +12,15 @@ import org.djunits.value.vdouble.scalar.base.DoubleScalar;
  * </p>
  * @author <a href="https://www.tudelft.nl/pknoppers">Peter Knoppers</a>
  */
-public interface UnitParser
+public interface QuantityParser
 {
     /**
-     * Parse a user-specific unit.
+     * Parse a user-specific quantity.
      * @param value the value (already parsed; expressed in the unit)
      * @param unit the unit specification
-     * @return the resulting (strongly typed) value, or null if the user-specified parser cannot handle
+     * @return the resulting (strongly typed) quantity, or null if the user-specified parser cannot handle
      *         the provided unit
      */
-    DoubleScalar<?, ?> parseUnit(final double value, final String unit);
+    Quantity<?> parseQuantity(final double value, final String unit);
 
 }
