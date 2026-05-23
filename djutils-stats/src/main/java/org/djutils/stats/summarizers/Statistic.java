5e70c08f9c9acb5dcba47fa5510c6b78f04a6ac4
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-05-23 12:03:00 +0200

    Update copyright year to 2026 for stats project

--- djutils-stats/src/main/java/org/djutils/stats/summarizers/Statistic.java
+++ djutils-stats/src/main/java/org/djutils/stats/summarizers/Statistic.java
@@ -3,7 +3,7 @@ package org.djutils.stats.summarizers;
 /**
  * The Statistic interface defines the methods to implement for each of the statistics classes.
  * <p>
- * Copyright (c) 2023-2025 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * Copyright (c) 2023-2026 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://simulation.tudelft.nl/" target="_blank"> https://simulation.tudelft.nl</a>. The DSOL
  * project is distributed under a three-clause BSD-style license, which can be found at
  * <a href="https://simulation.tudelft.nl/dsol/3.0/license.html" target="_blank">
--- djutils-stats/src/test/java/org/djutils/stats/summarizers/event/LoggingEventListener.java
+++ djutils-stats/src/test/java/org/djutils/stats/summarizers/event/LoggingEventListener.java
@@ -6,7 +6,7 @@ import org.djutils.event.EventListener;
 /**
  * LoggingEventListener logs the last event that was received and the number of received events so they can be checked. <br>
  * <br>
- * Copyright (c) 2020-2025 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * Copyright (c) 2020-2026 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
  * distributed under a three-clause BSD-style license, which can be found at
  * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>. <br>
