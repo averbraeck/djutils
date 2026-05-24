b13ca3e918cebd4e30f16f4bc47b950b09420aea
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-05-24 22:53:27 +0200

    Update Javadoc, replace </p> by <p>

--- djutils-math/src/main/java/org/djutils/math/complex/Complex.java
+++ djutils-math/src/main/java/org/djutils/math/complex/Complex.java
@@ -7,7 +7,7 @@ package org.djutils.math.complex;
  * <p>
  * Copyright (c) 2021-2026 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://djutils.org/docs/current/djutils/licenses.html">DJUTILS License</a>.
- * </p>
+ * <p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  * @author <a href="https://www.tudelft.nl/pknoppers">Peter Knoppers</a>
  */
--- djutils-math/src/main/java/org/djutils/math/functions/Sine.java
+++ djutils-math/src/main/java/org/djutils/math/functions/Sine.java
@@ -14,7 +14,7 @@ import org.djutils.math.AngleUtil;
  * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
  * distributed under a three-clause BSD-style license, which can be found at
  * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>.
- * </p>
+ * <p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
 public class Sine implements MathFunction
