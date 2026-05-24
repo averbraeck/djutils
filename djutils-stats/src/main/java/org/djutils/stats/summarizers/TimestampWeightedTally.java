35bbd76b7ae3dcf225dbb5ef3481dac1e3968662
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-05-24 22:53:52 +0200

    Update Javadoc, replace </p> by <p>

--- djutils-stats/src/main/java/org/djutils/stats/summarizers/TimestampWeightedTally.java
+++ djutils-stats/src/main/java/org/djutils/stats/summarizers/TimestampWeightedTally.java
@@ -14,7 +14,7 @@ import org.djutils.exceptions.Throw;
  * project is distributed under a three-clause BSD-style license, which can be found at
  * <a href="https://simulation.tudelft.nl/dsol/3.0/license.html" target="_blank">
  * https://simulation.tudelft.nl/dsol/3.0/license.html</a>.
- * </p>
+ * <p>
  * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank"> Alexander Verbraeck</a>
  */
 public class TimestampWeightedTally extends WeightedTally
