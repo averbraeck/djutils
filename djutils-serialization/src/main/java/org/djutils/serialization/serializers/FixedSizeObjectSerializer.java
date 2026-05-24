c709faf4a647e30f2d9151581509fb5bdf459127
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-05-24 22:59:37 +0200

    Issue #139. Update FixedObjectSerializer

--- djutils-serialization/src/main/java/org/djutils/serialization/serializers/FixedSizeObjectSerializer.java
+++ djutils-serialization/src/main/java/org/djutils/serialization/serializers/FixedSizeObjectSerializer.java
@@ -1,14 +1,16 @@
 package org.djutils.serialization.serializers;
 
+import org.djutils.serialization.FieldTypes;
+
 /**
  * Serializer for simple, fixed size, classes.
  * <p>
- * Copyright (c) 2019-2025 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * Copyright (c) 2019-2026 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://djutils.org/docs/current/djutils/licenses.html">DJUTILS License</a>.
- * </p>
+ * <p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  * @author <a href="https://www.tudelft.nl/staff/p.knoppers/">Peter Knoppers</a>
- * @param <T> class
+ * @param <T> class to be serialized
  */
 public abstract class FixedSizeObjectSerializer<T extends Object> extends ObjectSerializer<T>
 {
@@ -16,10 +18,10 @@ public abstract class FixedSizeObjectSerializer<T extends Object> extends Object
     private final int dataSize;
 
     /**
-     * Construct the FixedSizeSerializer.
-     * @param fieldType the field type (returned by the <code>fieldType</code> method)
-     * @param serializedDataSize number of bytes required for serialized T
-     * @param dataClassName returned by the dataClassName method
+     * Construct the FixedSizeObjectSerializer.
+     * @param fieldType the field type as defined by the {@link FieldTypes} class
+     * @param serializedDataSize number of bytes required for the serialized object
+     * @param dataClassName descriptive name of the type (not the class name)
      */
     public FixedSizeObjectSerializer(final byte fieldType, final int serializedDataSize, final String dataClassName)
     {
