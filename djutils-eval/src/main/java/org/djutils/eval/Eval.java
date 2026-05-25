14be7c83612d5c31bf27889a8adf1873f7e036d4
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-05-25 09:50:09 +0200

    Issue #142. Remove web pages from @author tags

--- djutils-eval/src/main/java/org/djutils/eval/Eval.java
+++ djutils-eval/src/main/java/org/djutils/eval/Eval.java
@@ -29,7 +29,7 @@ import org.djutils.metadata.ObjectDescriptor;
  * <a href="https://www.cs.bilkent.edu.tr/~guvenir/courses/CS101/op_precedence.html">Java Operator Precedence Table</a>,
  * skipping bitwise and other operators that make no sense for this evaluator and adding the exponentiation (^) operator.
  * <p>
- * @author <a href="https://www.tudelft.nl/pknoppers">Peter Knoppers</a>
+ * @author Peter Knoppers
  */
 public class Eval
 {
--- djutils-eval/src/main/java/org/djutils/eval/package-info.java
+++ djutils-eval/src/main/java/org/djutils/eval/package-info.java
@@ -4,8 +4,8 @@
  * Copyright (c) 2023-2026 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
  * <p>
- * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
- * @author <a href="https://www.tudelft.nl/pknoppers">Peter Knoppers</a>
- * @author <a href="https://www.transport.citg.tudelft.nl">Wouter Schakel</a>
+ * @author Alexander Verbraeck
+ * @author Peter Knoppers
+ * @author Wouter Schakel
  */
 package org.djutils.eval;
