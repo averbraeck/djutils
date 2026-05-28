315c8a8acad8f3561bdae43304363fa45dd4b50c
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-05-28 12:59:57 +0200

    Issue #143. Rename inner class to VECTOR

--- djutils-serialization/src/main/java/org/djutils/serialization/serializers/AbsVectorCodec.java
+++ djutils-serialization/src/main/java/org/djutils/serialization/serializers/AbsVectorCodec.java
@@ -36,7 +36,7 @@ public abstract class AbsVectorCodec extends BasicCodec<AbsVector<?, ?, ?, ?, ?>
     }
 
     /** Converter for Absolute Vector with a float value. */
-    protected static final BasicCodec<AbsVector<?, ?, ?, ?, ?>> CONVERT_ABS_QUANTITY_FLOAT =
+    protected static final BasicCodec<AbsVector<?, ?, ?, ?, ?>> CONVERT_ABS_VECTOR_FLOAT =
             new AbsVectorCodec(FieldTypes.FLOAT_32_UNIT_ABS_ARRAY)
             {
                 @Override
@@ -79,7 +79,7 @@ public abstract class AbsVectorCodec extends BasicCodec<AbsVector<?, ?, ?, ?, ?>
             };
 
     /** Converter for Absolute Vector with a double value. */
-    protected static final BasicCodec<AbsVector<?, ?, ?, ?, ?>> CONVERT_ABS_QUANTITY_DOUBLE =
+    protected static final BasicCodec<AbsVector<?, ?, ?, ?, ?>> CONVERT_ABS_VECTOR_DOUBLE =
             new AbsVectorCodec(FieldTypes.DOUBLE_64_UNIT_ABS_ARRAY)
             {
                 @Override
