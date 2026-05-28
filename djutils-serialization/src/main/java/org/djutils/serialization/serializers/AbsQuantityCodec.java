70d8c0c6f122a22e29b66eda6bf24ec0e91ab129
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-05-28 10:00:28 +0200

    Issue #1143. Codec for absolute quantities (float and double)

---
+++ djutils-serialization/src/main/java/org/djutils/serialization/serializers/AbsQuantityCodec.java
@@ -0,0 +1,101 @@
+package org.djutils.serialization.serializers;
+
+import org.djunits.quantity.def.AbsBasic;
+import org.djunits.quantity.def.Quantity;
+import org.djunits.unit.Unit;
+import org.djutils.serialization.Endianness;
+import org.djutils.serialization.FieldTypes;
+import org.djutils.serialization.SerializationException;
+
+/**
+ * (De)serializes a DJUNITS absolute Quantity.
+ * <p>
+ * Copyright (c) 2019-2026 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://djunits.org/docs/license.html">DJUNITS License</a>.
+ * <p>
+ * @author Alexander Verbraeck
+ */
+public abstract class AbsQuantityCodec extends BasicCodec<AbsBasic<?, ?, ?>>
+{
+    /**
+     * Construct the QuantityCodec.
+     * @param type the field type as defined by the {@link FieldTypes} class
+     */
+    public AbsQuantityCodec(final byte type)
+    {
+        super(type);
+    }
+
+    @Override
+    public int getNumberOfDimensions()
+    {
+        return 0;
+    }
+
+    /** Converter for Absolute Quantity with a float value. */
+    protected static final BasicCodec<AbsBasic<?, ?, ?>> CONVERT_ABS_QUANTITY_FLOAT =
+            new AbsQuantityCodec(FieldTypes.FLOAT_32_UNIT_ABS)
+            {
+                @Override
+                public int size(final AbsBasic<?, ?, ?> absQuantity) throws SerializationException
+                {
+                    return 2 + 4 + 1 + 4 + absQuantity.getReference().getId().length();
+                }
+
+                @Override
+                public void serialize(final AbsBasic<?, ?, ?> absQuantity, final byte[] buffer, final Pointer pointer,
+                        final Endianness endianness) throws SerializationException
+                {
+                    UnitCodec.encodeQuantityUnit(absQuantity.getQuantity(), buffer, pointer);
+                    StringCodec.CONVERT_STRING8.serializeWithPrefix(absQuantity.getReference().getId(), buffer, pointer,
+                            endianness);
+                    float v = (float) absQuantity.si();
+                    endianness.encodeDouble(v, buffer, pointer.getAndIncrement(4));
+                }
+
+                @Override
+                public AbsBasic<?, ?, ?> deserialize(final byte[] buffer, final Pointer pointer, final Endianness endianness)
+                        throws SerializationException
+                {
+                    Unit<?, ?> unit = UnitCodec.getUnit(buffer, pointer);
+                    String refStr = StringCodec.CONVERT_STRING8.deserialize(buffer, pointer, endianness);
+                    Quantity<?> quantity = unit.ofSi(endianness.decodeFloat(buffer, pointer.getAndIncrement(4)));
+                    UnitCodec.setDisplayUnit(quantity, unit);
+                    return AbsHelper.instantiateAbsQuantity(quantity, refStr);
+                }
+            };
+
+    /** Converter for Quantity with a double value. */
+    protected static final BasicCodec<AbsBasic<?, ?, ?>> CONVERT_ABS_QUANTITY_DOUBLE =
+            new AbsQuantityCodec(FieldTypes.DOUBLE_64_UNIT_ABS)
+            {
+                @Override
+                public int size(final AbsBasic<?, ?, ?> absQuantity) throws SerializationException
+                {
+                    return 2 + 8 + 1 + 4 + absQuantity.getReference().getId().length();
+                }
+
+                @Override
+                public void serialize(final AbsBasic<?, ?, ?> absQuantity, final byte[] buffer, final Pointer pointer,
+                        final Endianness endianness) throws SerializationException
+                {
+                    UnitCodec.encodeQuantityUnit(absQuantity.getQuantity(), buffer, pointer);
+                    StringCodec.CONVERT_STRING8.serializeWithPrefix(absQuantity.getReference().getId(), buffer, pointer,
+                            endianness);
+                    double v = absQuantity.si();
+                    endianness.encodeDouble(v, buffer, pointer.getAndIncrement(8));
+                }
+
+                @Override
+                public AbsBasic<?, ?, ?> deserialize(final byte[] buffer, final Pointer pointer, final Endianness endianness)
+                        throws SerializationException
+                {
+                    Unit<?, ?> unit = UnitCodec.getUnit(buffer, pointer);
+                    String refStr = StringCodec.CONVERT_STRING8.deserialize(buffer, pointer, endianness);
+                    Quantity<?> quantity = unit.ofSi(endianness.decodeDouble(buffer, pointer.getAndIncrement(8)));
+                    UnitCodec.setDisplayUnit(quantity, unit);
+                    return AbsHelper.instantiateAbsQuantity(quantity, refStr);
+                }
+            };
+
+}
